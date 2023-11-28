fb8efe21505183b888d5914fb80e2dfeaee01f41
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-11-28 01:59:14 +0000

    fixed plotting

--- R/CF_testing.r
+++ R/CF_testing.r
@@ -11,9 +11,9 @@ source("M4R_Clustering/R/Metrics.r")
 
 krange <- seq(from = 10, to = 300, by = 10)
 
-rmse_cos <- cross_val(data, 10, gen_cos_sim, krange)
-rmse_pcc <- cross_val(data, 10, gen_pcc_sim, krange)
+cos_scores <- cross_val(data, 10, gen_cos_sim, krange)
+pcc_scores <- cross_val(data, 10, gen_pcc_sim, krange)
 
-plot(krange, rmse_cos / 10, type = "l", col = "red", lwd = 2,
+plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
      ylim = c(1.03, 1.3))
-lines(krange, rmse_pcc / 10, type = "l", col = "blue", lwd = 2)
+lines(krange, pcc_scores$rmse, type = "l", col = "blue", lwd = 2)

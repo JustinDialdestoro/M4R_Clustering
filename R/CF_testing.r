7a3efbd1936647b68e121ef1b9cfa7d5e9207705
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-11-28 14:52:09 +0000

    test cosine

--- R/CF_testing.r
+++ R/CF_testing.r
@@ -12,8 +12,6 @@ source("M4R_Clustering/R/Metrics.r")
 krange <- seq(from = 10, to = 300, by = 10)
 
 cos_scores <- cross_val(data, 10, gen_cos_sim, krange)
-# pcc_scores <- cross_val(data, 10, gen_pcc_sim, krange)
 
 plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
      ylim = c(1.03, 1.3))
-# lines(krange, pcc_scores$rmse, type = "l", col = "blue", lwd = 2)

ad04f97486e1334f39507ac8431044bc80c29afe
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-11-29 00:49:13 +0000

    legends

--- R/CF_clust1_testing.r
+++ R/CF_clust1_testing.r
@@ -15,17 +15,20 @@ krange <- seq(from = 10, to = 300, by = 10)
 cos_scores_clust1 <- cross_val_cluster(data, 10, gen_cos_sim, krange)
 cos_scores <- cross_val(data, 10, gen_cos_sim, krange)
 
-plot(krange, cos_scores_clust1$rmse, type = "l", col = "blue", lwd = 2)
+plot(krange, cos_scores_clust1$rmse, type = "l", col = "blue", lwd = 2,
+     ylim = c(1.03, 1.25))
 lines(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2)
 legend("topright", c("No clustering", "User rating clustering"),
-       col = c("blue, red"), lwd = 2)
+       col = c("red", "blue"), lwd = 2)
 
-plot(krange, cos_scores_clust1$mae, type = "l", col = "blue", lwd = 2)
+plot(krange, cos_scores_clust1$mae, type = "l", col = "blue", lwd = 2,
+     ylim = c(0.83, 1))
 lines(krange, cos_scores$mae, type = "l", col = "red", lwd = 2)
 legend("topright", c("No clustering", "User rating clustering"),
-       col = c("blue, red"), lwd = 2)
+       col = c("red", "blue"), lwd = 2)
 
-plot(krange, cos_scores_clust1$r2, type = "l", col = "blue", lwd = 2)
+plot(krange, cos_scores_clust1$r2, type = "l", col = "blue", lwd = 2,
+     ylim = c(0.04, 0.15))
 lines(krange, cos_scores$r2, type = "l", col = "red", lwd = 2)
 legend("bottomright", c("No clustering", "User rating clustering"),
-       col = c("blue, red"), lwd = 2)
+       col = c("red", "blue"), lwd = 2)

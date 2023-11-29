e8003877e1fc6067c617620ffdb8b65f39eeb0b2
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-11-29 01:12:46 +0000

    data name

--- R/CF_clust1_testing.r
+++ R/CF_clust1_testing.r
@@ -1,10 +1,10 @@
-data <- read.table("M4R_Clustering/Data/u.data",
-                   col.names = c("userID", "filmID", "rating", "timestamp"))
+u100k <- read.table("M4R_Clustering/Data/u.data",
+                    col.names = c("userID", "filmID", "rating", "timestamp"))
 
-data2 <- read.csv("M4R_Clustering/Data/ratings.dat", sep = ":",
-                  colClasses = c(NA, "NULL"), header = FALSE)
+u1m <- read.csv("M4R_Clustering/Data/ratings.dat", sep = ":",
+                colClasses = c(NA, "NULL"), header = FALSE)
 
-colnames(data2) <- c("userID", "filmID", "rating", "timestamp")
+colnames(u1m) <- c("userID", "filmID", "rating", "timestamp")
 
 source("M4R_Clustering/R/CF_clust1.r")
 source("M4R_Clustering/R/CF.r")
@@ -12,8 +12,8 @@ source("M4R_Clustering/R/Metrics.r")
 
 krange <- seq(from = 10, to = 300, by = 10)
 
-cos_scores_c1 <- cross_val_c1(data, 10, gen_cos_sim, krange)
-cos_scores <- cross_val(data, 10, gen_cos_sim, krange)
+cos_scores_c1 <- cross_val_c1(u100k, 10, gen_cos_sim, krange)
+cos_scores <- cross_val(u100k, 10, gen_cos_sim, krange)
 
 plot(krange, cos_scores_c1$rmse, type = "l", col = "blue", lwd = 2,
      ylim = c(1.03, 1.25))
--- R/CF_testing.r
+++ R/CF_testing.r
@@ -1,17 +1,17 @@
-data <- read.table("M4R_Clustering/Data/u.data",
-                   col.names = c("userID", "filmID", "rating", "timestamp"))
+u100k <- read.table("M4R_Clustering/Data/u.data",
+                    col.names = c("userID", "filmID", "rating", "timestamp"))
 
-data2 <- read.csv("M4R_Clustering/Data/ratings.dat", sep = ":",
-                  colClasses = c(NA, "NULL"), header = FALSE)
+u1m <- read.csv("M4R_Clustering/Data/ratings.dat", sep = ":",
+                colClasses = c(NA, "NULL"), header = FALSE)
 
-colnames(data2) <- c("userID", "filmID", "rating", "timestamp")
+colnames(u1m) <- c("userID", "filmID", "rating", "timestamp")
 
 source("M4R_Clustering/R/CF.r")
 source("M4R_Clustering/R/Metrics.r")
 
 krange <- seq(from = 10, to = 300, by = 10)
 
-cos_scores <- cross_val(data, 10, gen_cos_sim, krange)
+cos_scores <- cross_val(u100k, 10, gen_cos_sim, krange)
 
 plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2)
 plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2)

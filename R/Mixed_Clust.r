8dedff42832d4cf98ac95f88f5c6409f0f0ac077
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-03 18:49:16 +0000

    added kprotoypes, mixed k means, and mskmeans

--- R/Mixed_Clust.r
+++ R/Mixed_Clust.r
@@ -1,6 +1,9 @@
 library("cluster")
 library("fastDummies")
 library("fpc")
+library("clustMixType")
+library("kmed")
+library("kamila")
 
 range_normalise <- function(x) {
   # normalise variable to a [0,1] range
@@ -47,4 +50,44 @@ hl_pam <- function(df, k) {
   dsim <- daisy(df, metric = "euclidean")
 
   return(pam(dsim, k = k)$clustering)
+}
+
+kprototypes <- function(df, k) {
+  # remove id and zip variable
+  df$userID <- NULL
+  df$zip <- NULL
+  # binarise gender variable
+  df$gender <- as.numeric(df$gender == "M")
+  # dummy code occupation variable
+  df$occupation <- as.factor(df$occupation)
+  return(kproto(df, k)$cluster)
+}
+
+mixed_k <- function(df, k) {
+  # remove id and zip variable
+  df$userID <- NULL
+  df$zip <- NULL
+  # binarise gender variable
+  df$gender <- as.numeric(df$gender == "M")
+  # dummy code occupation variable
+  df$occupation <- as.factor(df$occupation)
+
+  d <- distmix(df, method = "ahmad", idnum = 1, idbin = 2, idcat = 3)
+  return(fastkmed(d, k)$cluster)
+}
+
+mskmeans <- function(df, k) {
+  # remove zip variable
+  df$zip <- NULL
+
+  # dummy code gender and occupation variable
+  df <- dummy_cols(df, select_columns = "gender")
+  df$gender <- NULL
+
+  df <- dummy_cols(df, select_columns = "occupation")
+  df$occupation <- NULL
+
+  df$userID <- df$age
+
+  return(gmsClust(df[1:2], df[3:25], k)$results[1])
 }

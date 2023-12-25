6123c9768a130025aba2a2e983d56ee20f32c478
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-25 21:16:46 +0700

    original cosine function

--- R/Metrics.r
+++ R/Metrics.r
@@ -1,24 +1,24 @@
 library("recommenderlab")
 
+# gen_cos_sim <- function(ui) {
+#   sim <- similarity(as(ui, "realRatingMatrix"), method = "cosine",
+#                     which = "users")
+#   return(as(sim, "matrix"))
+# }
+
+# gen_pcc_sim <- function(ui) {
+#   sim <- similarity(as(ui, "realRatingMatrix"), method = "pearson",
+#                     which = "users")
+#   return(1 - as(sim, "matrix"))
+# }
+
+# gen_jacc_sim <- function(ui) {
+#   sim <- similarity(as(ui, "realRatingMatrix"), method = "jaccard",
+#                     which = "users")
+#   return(as(sim, "matrix"))
+# }
+
 gen_cos_sim <- function(ui) {
-  sim <- similarity(as(ui, "realRatingMatrix"), method = "cosine",
-                    which = "users")
-  return(as(sim, "matrix"))
-}
-
-gen_pcc_sim <- function(ui) {
-  sim <- similarity(as(ui, "realRatingMatrix"), method = "pearson",
-                    which = "users")
-  return(1 - as(sim, "matrix"))
-}
-
-gen_jacc_sim <- function(ui) {
-  sim <- similarity(as(ui, "realRatingMatrix"), method = "jaccard",
-                    which = "users")
-  return(as(sim, "matrix"))
-}
-
-gen_cos_sim_2 <- function(ui) {
   ui0 <- ui
   ui0[is.na(ui0)] <- 0
   sim <- ui0 %*% t(ui0)

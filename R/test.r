b5dc4aef98bfe6fd02795b914f56e35108d6c4b9
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-25 22:25:48 +0700

    timing tests

--- R/test.r
+++ R/test.r
@@ -1,35 +1,20 @@
-cosine_similarity <- function(vector1, vector2) {
-  # Filter out missing values (NAs)
-  valid_indices <- which(!is.na(vector1) & !is.na(vector2))
-  v1 <- vector1[valid_indices]
-  v2 <- vector2[valid_indices]
-  
-  # Compute cosine similarity
-  if (length(v1) == 0 || length(v2) == 0) {
-    return(0)  # Return 0 if no overlap in ratings
-  } else {
-    return(sum(v1 * v2) / (sqrt(sum(v1^2)) * sqrt(sum(v2^2))))
-  }
-}
+source("M4R_Clustering/R/CF.r")
+source("M4R_Clustering/R/Metrics.r")
 
-user_item_matrix <- matrix(
-  c(1, NA, 3, 2, 3, NA, NA, 4, 1, 3, 1, NA, NA, 4, NA, 5),
-  nrow = 4,
-  byrow = TRUE,
-  dimnames = list(c("User1", "User2", "User3", "User4"), c("Item1", "Item2", "Item3", "Item4"))
-)
+u100k <- read.table("M4R_Clustering/Data/u.data",
+                    col.names = c("userID", "filmID", "rating", "timestamp"))
 
-similarity_matrix <- matrix(nrow = 4, ncol = 4)
+t1 <- Sys.time()
+f_ind <- t_fold_index(u100k, 10)
+f <- t_fold(u100k, f_ind)
+t2 <- Sys.time()
 
-for (i in 1:4) {
-  for (j in 1:4) {
-    similarity_matrix[i, j] <- cosine_similarity(user_item_matrix[i, ], user_item_matrix[j, ])
-  }
-}
+t1 <- Sys.time()
+ui <- gen_ui_matrix(u100k, f[[1]])
+t2 <- Sys.time()
 
-# Set row and column names
-rownames(similarity_matrix) <- colnames(similarity_matrix) <- c("User1", "User2", "User3", "User4")
+t1 <- Sys.time()
+sim <- gen_cos_sim((ui))
+t2 <- Sys.time()
 
-sim <- similarity(as(user_item_matrix, "realRatingMatrix"), method = "cosine",
-                    which = "users")
-s <- as(sim, "matrix")
+print(t2-t1)

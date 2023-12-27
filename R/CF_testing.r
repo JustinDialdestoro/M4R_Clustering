94dfc792e4528ec0ae02f5c32c962ae385d29b74
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-27 22:48:47 +0700

    jaccard and chebyshev plots

--- R/CF_testing.r
+++ R/CF_testing.r
@@ -18,37 +18,45 @@ krange <- seq(from = 10, to = 300, by = 10)
 cos_scores <- cross_val(u100k, 10, gen_cos_sim, krange)
 acos_scores <- cross_val(u100k, 10, gen_acos_sim, krange)
 pcc_scores <- cross_val(u100k, 10, gen_pcc_sim, krange)
+jacc_scores <- cross_val(u100k, 10, gen_jacc_sim, krange)
 euc_scores <- cross_val(u100k, 10, gen_euc_sim, krange)
 mhat_scores <- cross_val(u100k, 10, gen_mhat_sim, krange)
+cheb_scores <- cross_val(u100k, 10, gen_cheb_sim, krange)
 
 library("viridis")
 
-plot(krange, cos_scores$rmse, type = "l", col = viridis(5)[1], lwd = 2,
+plot(krange, cos_scores$rmse, type = "l", col = viridis(7)[1], lwd = 2,
      ylim = c(0.955, 1.05))
-lines(krange, acos_scores$rmse, type = "l", col = viridis(5)[2], lwd = 2)
-lines(krange, pcc_scores$rmse, type = "l", col = viridis(5)[3], lwd = 2)
-lines(krange, euc_scores$rmse, type = "l", col = viridis(5)[4], lwd = 2)
-lines(krange, mhat_scores$rmse, type = "l", col = viridis(5)[5], lwd = 2)
+lines(krange, acos_scores$rmse, type = "l", col = viridis(7)[2], lwd = 2)
+lines(krange, pcc_scores$rmse, type = "l", col = viridis(7)[3], lwd = 2)
+lines(krange, jacc_scores$rmse, type = "l", col = viridis(7)[4], lwd = 2)
+lines(krange, euc_scores$rmse, type = "l", col = viridis(7)[5], lwd = 2)
+lines(krange, mhat_scores$rmse, type = "l", col = viridis(7)[6], lwd = 2)
+lines(krange, cheb_scores$rmse, type = "l", col = viridis(7)[7], lwd = 2)
 legend("bottomright", c("cosine", "adjusted cosine", "pearson's correlation",
-                        "euclidean", "manhattan"),
-       col = viridis(5), lwd = 2, cex = 1)
+                        "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lwd = 2, cex = 1)
 
-plot(krange, cos_scores$mae, type = "l", col = viridis(5)[1], lwd = 2,
-     ylim = c(0.75, 0.825))
-lines(krange, acos_scores$mae, type = "l", col = viridis(5)[2], lwd = 2)
-lines(krange, pcc_scores$mae, type = "l", col = viridis(5)[3], lwd = 2)
-lines(krange, euc_scores$mae, type = "l", col = viridis(5)[4], lwd = 2)
-lines(krange, mhat_scores$mae, type = "l", col = viridis(5)[5], lwd = 2)
+plot(krange, cos_scores$mae, type = "l", col = viridis(7)[1], lwd = 2,
+     ylim = c(0.955, 1.05))
+lines(krange, acos_scores$mae, type = "l", col = viridis(7)[2], lwd = 2)
+lines(krange, pcc_scores$mae, type = "l", col = viridis(7)[3], lwd = 2)
+lines(krange, jacc_scores$mae, type = "l", col = viridis(7)[4], lwd = 2)
+lines(krange, euc_scores$mae, type = "l", col = viridis(7)[5], lwd = 2)
+lines(krange, mhat_scores$mae, type = "l", col = viridis(7)[6], lwd = 2)
+lines(krange, cheb_scores$mae, type = "l", col = viridis(7)[7], lwd = 2)
 legend("bottomright", c("cosine", "adjusted cosine", "pearson's correlation",
-                        "euclidean", "manhattan"),
-       col = viridis(5), lwd = 2, cex = 1)
+                        "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lwd = 2, cex = 1)
 
-plot(krange, cos_scores$r2, type = "l", col = viridis(5)[1], lwd = 2,
-     ylim = c(0.16, 0.28))
-lines(krange, acos_scores$r2, type = "l", col = viridis(5)[2], lwd = 2)
-lines(krange, pcc_scores$r2, type = "l", col = viridis(5)[3], lwd = 2)
-lines(krange, euc_scores$r2, type = "l", col = viridis(5)[4], lwd = 2)
-lines(krange, mhat_scores$r2, type = "l", col = viridis(5)[5], lwd = 2)
-legend("topright", c("cosine", "adjusted cosine", "pearson's correlation",
-                     "euclidean", "manhattan"),
-       col = viridis(5), lwd = 2, cex = 1)
+plot(krange, cos_scores$r2, type = "l", col = viridis(7)[1], lwd = 2,
+     ylim = c(0.955, 1.05))
+lines(krange, acos_scores$r2, type = "l", col = viridis(7)[2], lwd = 2)
+lines(krange, pcc_scores$r2, type = "l", col = viridis(7)[3], lwd = 2)
+lines(krange, jacc_scores$r2, type = "l", col = viridis(7)[4], lwd = 2)
+lines(krange, euc_scores$r2, type = "l", col = viridis(7)[5], lwd = 2)
+lines(krange, mhat_scores$r2, type = "l", col = viridis(7)[6], lwd = 2)
+lines(krange, cheb_scores$r2, type = "l", col = viridis(7)[7], lwd = 2)
+legend("bottomright", c("cosine", "adjusted cosine", "pearson's correlation",
+                        "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lwd = 2, cex = 1)

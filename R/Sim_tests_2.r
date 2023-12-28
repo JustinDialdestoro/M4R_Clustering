6dcfebdcfb3e9ce86a0cc17b8e457e8cc1e80838
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-29 00:51:10 +0700

    euc predictor plots

--- R/Sim_tests_2.r
+++ R/Sim_tests_2.r
@@ -24,25 +24,25 @@ euc_scores_4 <- cross_val(u100k, 10, gen_euc_sim, discrete, krange)
 library("viridis")
 
 plot(krange, euc_scores_1$rmse, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.955, 1.05))
+     ylim = c(0.95, 1.105))
 lines(krange, euc_scores_2$rmse, type = "l", col = viridis(4)[2], lwd = 2)
 lines(krange, euc_scores_3$rmse, type = "l", col = viridis(4)[3], lwd = 2)
 lines(krange, euc_scores_4$rmse, type = "l", col = viridis(4)[4], lwd = 2)
-legend("bottomright", c("weighted sum", "mean centered", "z score", "discrete"),
+legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lwd = 2, cex = 1)
 
 plot(krange, euc_scores_1$mae, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.75, 0.825))
+     ylim = c(0.74, 0.8))
 lines(krange, euc_scores_2$mae, type = "l", col = viridis(4)[2], lwd = 2)
 lines(krange, euc_scores_3$mae, type = "l", col = viridis(4)[3], lwd = 2)
 lines(krange, euc_scores_4$mae, type = "l", col = viridis(4)[4], lwd = 2)
-legend("bottomright", c("weighted sum", "mean centered", "z score", "discrete"),
+legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lwd = 2, cex = 1)
 
 plot(krange, euc_scores_1$r2, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.165, 0.28))
+     ylim = c(0.15, 0.29))
 lines(krange, euc_scores_2$r2, type = "l", col = viridis(4)[2], lwd = 2)
 lines(krange, euc_scores_3$r2, type = "l", col = viridis(4)[3], lwd = 2)
 lines(krange, euc_scores_4$r2, type = "l", col = viridis(4)[4], lwd = 2)
-legend("bottomright", c("weighted sum", "mean centered", "z score", "discrete"),
+legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lwd = 2, cex = 1)

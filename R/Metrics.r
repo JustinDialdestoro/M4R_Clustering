5203a956e1bac07a135b73b903a9b3e288ee1b2b
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2023-12-30 01:16:05 +0700

    clustering metrics

--- R/Metrics.r
+++ R/Metrics.r
@@ -92,3 +92,111 @@ gen_cheb_sim <- function(ui) {
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
   return(1 / (1 + sim))
 }
+
+cos_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      clust_dist[i, j] <- cosine(ui[i, ], centres[[j]])
+    }
+  }
+  return(clust_dist)
+}
+
+acos_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  mean <- rowMeans(t(ui), na.rm = TRUE)
+  ui0 <- t(t(ui) - mean)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      clust_dist[i, j] <- cosine(ui0[i, ], centres[[j]] - mean)
+    }
+  }
+  return(clust_dist)
+}
+
+pcc_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  mean <- rowMeans(ui, na.rm = TRUE)
+  ui0 <- ui - mean
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      clust_dist[i, j] <- cosine(ui0[i, ], centres[[j]] -
+                                   mean(centres[[j]], na.rm = TRUE))
+    }
+  }
+  return(clust_dist)
+}
+
+jacc_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      num <- which(!is.na(ui[i, ]) & !is.na(centres[[j]]))
+      denom <- which(!is.na(ui[i, ]) | !is.na(centres[[j]]))
+      clust_dist[i, j] <- length(num) / length(denom)
+    }
+  }
+  return(clust_dist)
+}
+
+euc_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      ind <- which(!is.na(ui[i, ]) & !is.na(centres[[j]]))
+      if (length(ind) == 0) {
+        clust_dist[i, j] <- Inf
+      } else {
+        clust_dist[i, j] <- norm(ui[i, ][ind] - centres[[j]][ind], type = "2")
+      }
+    }
+  }
+  return(1 / (1 + clust_dist))
+}
+
+mhat_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      ind <- which(!is.na(ui[i, ]) & !is.na(centres[[j]]))
+      if (length(ind) == 0) {
+        clust_dist[i, j] <- Inf
+      } else {
+        clust_dist[i, j] <- sum(abs(ui[i, ][ind] - centres[[j]][ind]))
+      }
+    }
+  }
+  return(1 / (1 + clust_dist))
+}
+
+cheb_clust <- function(ui, centres) {
+  n <- nrow(ui)
+  clust_dist <- matrix(NA, nrow = n, ncol = 3)
+
+  for (i in 1:n) {
+    for (j in 1:3) {
+      ind <- which(!is.na(ui[i, ]) & !is.na(centres[[j]]))
+      if (length(ind) == 0) {
+        clust_dist[i, j] <- Inf
+      } else {
+        clust_dist[i, j] <- max(abs(ui[i, ][ind] - centres[[j]][ind]))
+      }
+    }
+  }
+  return(1 / (1 + clust_dist))
+}

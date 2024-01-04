141a60967691d239ac19c32f7a4823560e092653
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-04 20:45:07 +0700

    euc user cluster testing

--- R/CF_clust1_testing.r
+++ R/CF_clust1_testing.r
@@ -9,26 +9,9 @@ colnames(u1m) <- c("userID", "filmID", "rating", "timestamp")
 source("M4R_Clustering/R/CF_clust1.r")
 source("M4R_Clustering/R/CF.r")
 source("M4R_Clustering/R/Metrics.r")
+source("M4R_Clustering/R/Metrics.r")
 
 krange <- seq(from = 10, to = 300, by = 10)
 
-cos_scores_c1 <- cross_val_c1(u100k, 10, gen_cos_sim, krange)
-cos_scores <- cross_val(u100k, 10, gen_cos_sim, krange)
-
-plot(krange, cos_scores_c1$rmse, type = "l", col = "blue", lwd = 2,
-     ylim = c(1.03, 1.25))
-lines(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2)
-legend("topright", c("No clustering", "User rating clustering"),
-       col = c("red", "blue"), lwd = 2)
-
-plot(krange, cos_scores_c1$mae, type = "l", col = "blue", lwd = 2,
-     ylim = c(0.83, 1))
-lines(krange, cos_scores$mae, type = "l", col = "red", lwd = 2)
-legend("topright", c("No clustering", "User rating clustering"),
-       col = c("red", "blue"), lwd = 2)
-
-plot(krange, cos_scores_c1$r2, type = "l", col = "blue", lwd = 2,
-     ylim = c(0.04, 0.15))
-lines(krange, cos_scores$r2, type = "l", col = "red", lwd = 2)
-legend("bottomright", c("No clustering", "User rating clustering"),
-       col = c("red", "blue"), lwd = 2)
+euc_scores_1 <- cross_val_clust1(u100k, 10, krange,
+                                 gen_euc_sim, z_score, euc_clust)

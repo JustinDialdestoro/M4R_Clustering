cb63e45753e294f3377702617c8fb044e8b733d4
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-06 01:34:21 +0700

    acos and euc plots

--- R Code/Clustering/Rating_clustering_results.r
+++ R Code/Clustering/Rating_clustering_results.r
@@ -13,33 +13,50 @@ source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 
 nrange <- seq(from = 2, to = 10)
 
-euc_scores <- cval_rating_clust(u100k, 10, 30, nrange,
-                                gen_euc_sim, weighted_sum)
+# acos_scores <- cval_rating_clust(u100k, 10, 30, nrange,
+#                                  gen_acos_sim, weighted_sum)
+# euc_scores <- cval_rating_clust(u100k, 10, 30, nrange,
+#                                 gen_euc_sim, weighted_sum)
 
 library("viridis")
 
-scores <- euc_scores
+scores <- rbind(acos_scores, euc_scores)
 
 ymax <- max(scores$rmse)
 ymin <- min(scores$rmse)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(nrange, euc_scores$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
-     col = viridis(4)[1], xlab = "n clusters", ylab = "RMSE",
+plot(nrange, acos_scores$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(2)[1], xlab = "n clusters", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, euc_scores$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(2)[2])
+legend("bottomleft",
+       c("adjusted cosine", "euclidean"),
+       col = viridis(2), lty = 2, pch = 4, lwd = 2, cex = 1)
 
 ymax <- max(scores$mae)
 ymin <- min(scores$mae)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(nrange, euc_scores$mae, lty = 2, type = "b", pch = 4, lwd = 2,
-     col = viridis(4)[1], xlab = "n clusters", ylab = "MAE",
+plot(nrange, acos_scores$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(2)[1], xlab = "n clusters", ylab = "MAE",
      ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, euc_scores$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(2)[2])
+legend("bottomleft",
+       c("adjusted cosine", "euclidean"),
+       col = viridis(2), lty = 2, pch = 4, lwd = 2, cex = 1)
 
 ymax <- max(scores$r2)
 ymin <- min(scores$r2)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(nrange, euc_scores$r2, lty = 2, type = "b", pch = 4, lwd = 2,
-     col = viridis(4)[1], xlab = "n clusters", ylab = "R2",
+plot(nrange, acos_scores$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(2)[1], xlab = "n clusters", ylab = "R2",
      ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, euc_scores$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(2)[2])
+legend("bottomleft",
+       c("adjusted cosine", "euclidean"),
+       col = viridis(2), lty = 2, pch = 4, lwd = 2, cex = 1)

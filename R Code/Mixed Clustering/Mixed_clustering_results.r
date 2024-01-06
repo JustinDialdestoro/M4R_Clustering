7228c662e57c8ab26d89396a0d4eaeb64f8ccfdd
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-06 21:03:39 +0700

    sim gow clust plots

--- R Code/Mixed Clustering/Mixed_clustering_results.r
+++ R Code/Mixed Clustering/Mixed_clustering_results.r
@@ -10,229 +10,99 @@ udem <- read.table("M4R_Clustering/Data/u.user", sep = "|",
                    col.names = c("userID", "age", "gender",
                                  "occupation", "zip"))
 
+source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
+source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 source("M4R_Clustering/R Code/Mixed Clustering/Mixed_clustering_functions.r")
 source("M4R_Clustering/R Code/Mixed Clustering/Mixed_clustering.r")
 
-krange <- seq(from = 10, to = 300, by = 10)
+nrange <- seq(from = 2, to = 10)
 
-cos_scores <- cross_val(u100k, 10, gen_cos_sim, krange)
+cos_scores_gow <- cval_mixed_clust(u100k, udem, 10, 40, nrange,
+                                   gen_cos_sim, weighted_sum, gow_pam)
+acos_scores_gow <- cval_mixed_clust(u100k, udem, 10, 30, nrange,
+                                    gen_acos_sim, weighted_sum, gow_pam)
+pcc_scores_gow <- cval_mixed_clust(u100k, udem, 10, 40, nrange,
+                                   gen_pcc_sim, weighted_sum, gow_pam)
+jacc_scores_gow <- cval_mixed_clust(u100k, udem, 10, 30, nrange,
+                                    gen_jacc_sim, weighted_sum, gow_pam)
+euc_scores_gow <- cval_mixed_clust(u100k, udem, 10, 30, nrange,
+                                   gen_euc_sim, weighted_sum, gow_pam)
+mhat_scores_gow <- cval_mixed_clust(u100k, udem, 10, 20, nrange,
+                                    gen_mhat_sim, weighted_sum, gow_pam)
+cheb_scores_gow <- cval_mixed_clust(u100k, udem, 10, 30, nrange,
+                                    gen_cheb_sim, weighted_sum, gow_pam)
 
-cos_scores_gow_2 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-                                 gow_pam, udem, 2)
-cos_scores_gow_3 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-                                 gow_pam, udem, 3)
-cos_scores_gow_4 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-                                 gow_pam, udem, 4)
-cos_scores_gow_5 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-                                 gow_pam, udem, 5)
-cos_scores_gow_6 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-                                 gow_pam, udem, 6)
+library("viridis")
 
-plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
-     ylim = c(1.01, 1.065))
-lines(krange, cos_scores_gow_2$rmse, type = "l", col = "#020294", lwd = 2)
-lines(krange, cos_scores_gow_3$rmse, type = "l", col = "blue", lwd = 2)
-lines(krange, cos_scores_gow_4$rmse, type = "l", col = "#006eff", lwd = 2)
-lines(krange, cos_scores_gow_5$rmse, type = "l", col = "#00aaff", lwd = 2)
-lines(krange, cos_scores_gow_6$rmse, type = "l", col = "#00fff7", lwd = 2)
-legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-       col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-       lwd = 2, cex = 0.8)
+scores <- rbind(cos_scores_gow, acos_scores_gow, pcc_scores_gow,
+                jacc_scores_gow, euc_scores_gow, mhat_scores_gow,
+                cheb_scores_gow)
 
-plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2,
-     ylim = c(0.8, 0.835))
-lines(krange, cos_scores_gow_2$mae, type = "l", col = "#020294", lwd = 2)
-lines(krange, cos_scores_gow_3$mae, type = "l", col = "blue", lwd = 2)
-lines(krange, cos_scores_gow_4$mae, type = "l", col = "#006eff", lwd = 2)
-lines(krange, cos_scores_gow_5$mae, type = "l", col = "#00aaff", lwd = 2)
-lines(krange, cos_scores_gow_6$mae, type = "l", col = "#00fff7", lwd = 2)
-legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-       col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-       lwd = 2, cex = 0.8)
+ymax <- max(scores$rmse)
+ymin <- min(scores$rmse)
+ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, cos_scores$r2, type = "l", col = "red", lwd = 2,
-     ylim = c(0.15, 0.205))
-lines(krange, cos_scores_gow_2$r2, type = "l", col = "#020294", lwd = 2)
-lines(krange, cos_scores_gow_3$r2, type = "l", col = "blue", lwd = 2)
-lines(krange, cos_scores_gow_4$r2, type = "l", col = "#006eff", lwd = 2)
-lines(krange, cos_scores_gow_5$r2, type = "l", col = "#00aaff", lwd = 2)
-lines(krange, cos_scores_gow_6$r2, type = "l", col = "#00fff7", lwd = 2)
-legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-       col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-       lwd = 2, cex = 0.8)
+plot(nrange, cos_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(7)[1], xlab = "n clusters", ylab = "RMSE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, acos_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[2])
+lines(nrange, pcc_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[3])
+lines(nrange, jacc_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[4])
+lines(nrange, euc_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[5])
+lines(nrange, mhat_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[6])
+lines(nrange, cheb_scores_gow$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[7])
+legend("bottomright", c("cosine", "adjusted cosine", "pearson's correlation",
+                        "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lty = 2, pch = 4, lwd = 2, cex = 0.8)
 
-# cos_scores_hl_2 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                 hl_pam, udem, 2)
-# cos_scores_hl_3 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                 hl_pam, udem, 3)
-# cos_scores_hl_4 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                 hl_pam, udem, 4)
-# cos_scores_hl_5 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                 hl_pam, udem, 5)
-# cos_scores_hl_6 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                 hl_pam, udem, 6)
+ymax <- max(scores$mae)
+ymin <- min(scores$mae)
+ygap <- 0.2 * (ymax - ymin)
 
-# plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
-#      ylim = c(1.03, 1.25))
-# lines(krange, cos_scores_hl_2$rmse, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_hl_3$rmse, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_hl_4$rmse, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_hl_5$rmse, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_hl_6$rmse, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
+plot(nrange, cos_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(7)[1], xlab = "n clusters", ylab = "MAE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, acos_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[2])
+lines(nrange, pcc_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[3])
+lines(nrange, jacc_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[4])
+lines(nrange, euc_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[5])
+lines(nrange, mhat_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[6])
+lines(nrange, cheb_scores_gow$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[7])
+legend("bottomright", c("cosine", "adjusted cosine", "pearson's correlation",
+                        "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lty = 2, pch = 4, lwd = 2, cex = 0.8)
 
-# plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.83, 1))
-# lines(krange, cos_scores_hl_2$mae, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_hl_3$mae, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_hl_4$mae, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_hl_5$mae, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_hl_6$mae, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
+ymax <- max(scores$r2)
+ymin <- min(scores$r2)
+ygap <- 0.2 * (ymax - ymin)
 
-# plot(krange, cos_scores$r2, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.04, 0.15))
-# lines(krange, cos_scores_hl_2$r2, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_hl_3$r2, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_hl_4$r2, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_hl_5$r2, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_hl_6$r2, type = "l", col = "#00fff7", lwd = 2)
-# legend("bottomright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# cos_scores_kproto_2 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                     kprototypes, udem, 2)
-# cos_scores_kproto_3 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                     kprototypes, udem, 3)
-# cos_scores_kproto_4 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                     kprototypes, udem, 4)
-# cos_scores_kproto_5 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                     kprototypes, udem, 5)
-# cos_scores_kproto_6 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                     kprototypes, udem, 6)
-
-# plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
-#      ylim = c(1.03, 1.25))
-# lines(krange, cos_scores_kproto_2$rmse, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_kproto_3$rmse, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_kproto_4$rmse, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_kproto_5$rmse, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_kproto_6$rmse, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.83, 1))
-# lines(krange, cos_scores_kproto_2$mae, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_kproto_3$mae, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_kproto_4$mae, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_kproto_5$mae, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_kproto_6$mae, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$r2, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.04, 0.15))
-# lines(krange, cos_scores_kproto_2$r2, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_kproto_3$r2, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_kproto_4$r2, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_kproto_5$r2, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_kproto_6$r2, type = "l", col = "#00fff7", lwd = 2)
-# legend("bottomright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# cos_scores_mixed_2 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                    mixed_k, udem, 2)
-# cos_scores_mixed_3 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                    mixed_k, udem, 3)
-# cos_scores_mixed_4 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                    mixed_k, udem, 4)
-# cos_scores_mixed_5 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                    mixed_k, udem, 5)
-# cos_scores_mixed_6 <- cross_val_c2(u100k, 10, gen_cos_sim, krange,
-#                                    mixed_k, udem, 6)
-
-# plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
-#      ylim = c(1.03, 1.25))
-# lines(krange, cos_scores_mixed_2$rmse, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_mixed_3$rmse, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_mixed_4$rmse, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_mixed_5$rmse, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_mixed_6$rmse, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.83, 1))
-# lines(krange, cos_scores_mixed_2$mae, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_mixed_3$mae, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_mixed_4$mae, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_mixed_5$mae, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_mixed_6$mae, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$r2, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.04, 0.15))
-# lines(krange, cos_scores_mixed_2$r2, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_mixed_3$r2, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_mixed_4$r2, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_mixed_5$r2, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_mixed_6$r2, type = "l", col = "#00fff7", lwd = 2)
-# legend("bottomright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# cos_scores_gow_2 <- cross_val_c2(u100k, 10, gen_cos_sim_2, krange,
-#                                 gow_pam, udem, 2)
-# cos_scores_gow_3 <- cross_val_c2(u100k, 10, gen_cos_sim_2, krange,
-#                                 gow_pam, udem, 3)
-# cos_scores_gow_4 <- cross_val_c2(u100k, 10, gen_cos_sim_2, krange,
-#                                 gow_pam, udem, 4)
-# cos_scores_gow_5 <- cross_val_c2(u100k, 10, gen_cos_sim_2, krange,
-#                                 gow_pam, udem, 5)
-# cos_scores_gow_6 <- cross_val_c2(u100k, 10, gen_cos_sim_2, krange,
-#                                 gow_pam, udem, 6)
-
-# plot(krange, cos_scores$rmse, type = "l", col = "red", lwd = 2,
-#      ylim = c(1.005, 1.08))
-# lines(krange, cos_scores_gow_2$rmse, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_gow_3$rmse, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_gow_4$rmse, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_gow_5$rmse, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_gow_6$rmse, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$mae, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.8, 0.85))
-# lines(krange, cos_scores_gow_2$mae, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_gow_3$mae, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_gow_4$mae, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_gow_5$mae, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_gow_6$mae, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
-
-# plot(krange, cos_scores$r2, type = "l", col = "red", lwd = 2,
-#      ylim = c(0.14, 0.21))
-# lines(krange, cos_scores_gow_2$r2, type = "l", col = "#020294", lwd = 2)
-# lines(krange, cos_scores_gow_3$r2, type = "l", col = "blue", lwd = 2)
-# lines(krange, cos_scores_gow_4$r2, type = "l", col = "#006eff", lwd = 2)
-# lines(krange, cos_scores_gow_5$r2, type = "l", col = "#00aaff", lwd = 2)
-# lines(krange, cos_scores_gow_6$r2, type = "l", col = "#00fff7", lwd = 2)
-# legend("topright", c("No clustering", "k=2", "k=3", "k=4", "k=5", "k=6"),
-#        col = c("red", "#020294", "blue", "#006eff", "#00aaff", "#00fff7"),
-#        lwd = 2)
+plot(nrange, cos_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(7)[1], xlab = "n clusters", ylab = "R2",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(nrange, acos_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[2])
+lines(nrange, pcc_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[3])
+lines(nrange, jacc_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[4])
+lines(nrange, euc_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[5])
+lines(nrange, mhat_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[6])
+lines(nrange, cheb_scores_gow$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(7)[7])
+legend("topright", c("cosine", "adjusted cosine", "pearson's correlation",
+                     "jaccard", "euclidean", "manhattan", "chebyshev"),
+       col = viridis(7), lty = 2, pch = 4, lwd = 2, cex = 0.8)

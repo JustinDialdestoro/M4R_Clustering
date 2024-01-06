7cfea7072a39bd5fe886c6fd960c2d3bd267d6ad
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-06 20:05:00 +0700

    mixed cluster cross validation

--- R Code/Mixed Clustering/Mixed_clustering.r
+++ R Code/Mixed Clustering/Mixed_clustering.r
@@ -1,71 +1,59 @@
 source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
+source("M4R_Clustering/R Code/Clustering/Rating_preference_clustering.r")
 
-knn_c2 <- function(ui, sim, k, userid, filmid, clusters) {
-  ind <- which((ui[, filmid] > 0) & (clusters[1, ] == clusters[1, userid]))
-  neighbours <- na.omit(ind[order(-sim[userid, ][ind])[1:k]])
+cval_rating_clust <- function(df, df_feat, t, k, n_range, metric,
+                              pred_func, clust_func) {
+  m <- length(n_range)
+  # initial scores table
+  scores <- data.frame(rmse = rep(0, m), mae = rep(0, m), r2 = rep(0, m))
 
-  nclust <- dim(clusters)[1]
-  i <- 2
-  while (length(neighbours) < k) {
-    if (i > nclust) {
-      break
-    }
-
-    ind <- which((ui[, filmid] > 0) & (clusters[i, ] == clusters[i, userid]))
-    neighbours <- c(neighbours,
-                    na.omit(ind[order(-sim[userid, ][ind])[1:k]]))
-    i <- i + 1
-  }
-
-  return(neighbours)
-}
-
-pred_ratings_c2 <- function(df, predid, ui, sim, k, clusters) {
-  userid <- df$userID[predid]
-  filmid <- df$filmID[predid]
-
-  neighbours <- knn_c2(ui, sim, k, userid, filmid, clusters)
-
-  num <- sim[neighbours, userid] %*% ui[neighbours, filmid]
-  denom <- sum(abs(sim[neighbours, userid])) + 1e-9
-
-  return(num / denom)
-}
-
-pred_fold_c2 <- function(df, df_ind, ui, sim, k, clusters) {
-  preds <- c()
-  for (p in df_ind) {
-    preds <- c(preds, pred_ratings_c2(df, p, ui, sim, k, clusters))
-  }
-  return(preds)
-}
-
-vary_k_c2 <- function(df, ui, sim, test_ind, k_range, scores, clusters) {
-  for (k in seq_along(k_range)) {
-
-    r_pred <- pred_fold_c2(df, test_ind, ui, sim, k_range[k], clusters)
-    r_true <- df$rating[test_ind]
-
-    scores$rmse[k] <- scores$rmse[k] + rmse(r_pred, r_true) # nolint
-    scores$mae[k] <- scores$mae[k] + mae(r_pred, r_true) # nolint
-    scores$r2[k] <- scores$r2[k] + r2(r_pred, r_true) # nolint
-  }
-  return(scores)
-}
-
-cross_val_c2 <- function(df, t, metric, k_range, clust_method, features, k) {
-  n <- length(k_range)
-  scores <- data.frame(rmse = rep(0, n), mae = rep(0, n), r2 = rep(0, n))
+  # t-fold creation
   cval_f_i <- t_fold_index(df, t) # nolint
   cval_f <- t_fold(df, cval_f_i) # nolint
 
+  # loop over each fold
   for (i in 1:t) {
-    ui <- gen_ui_matrix(df, cval_f_i[[i]]) # nolint
-    sim <- metric(ui)
 
-    c <- clust_method(features, k)
+    # loop over every n
+    for (n in seq_along(n_range)) {
+      print(paste("Offline phase for n = ", n_range[n], ", fold", i, ":"))
+      t1 <- Sys.time()
 
-    scores <- vary_k_c2(df, ui, sim, cval_f_i[[i]], k_range, scores, c)
+      # ui and similarity matrix
+      ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
+
+      # create rating clusters
+      clusters <- clust_func(df_feat, n_range[n])
+
+      # segment user ratings matrix into the n clusters
+      uis <- replicate(n_range[n], c())
+      for (j in 1:n_range[n]) {
+        uis[[j]] <- ui[which(clusters == j), ]
+      }
+
+      # similarity matrix for each segmented ui matrix
+      sims <- replicate(n_range[n], c())
+      for (j in 1:n_range[n]) {
+        sims[[j]] <- metric(uis[[j]])
+      }
+
+      print(Sys.time() - t1)
+
+      print("Online phase:")
+      t1 <- Sys.time()
+
+      # predict on test fold ratings
+      r_pred <- pred_fold_clust(df, cval_f_i[[i]], uis, sims, pred_func, # nolint
+                                k, clusters)
+      r_true <- df$rating[cval_f_i[[i]]]
+
+      # error metrics
+      scores$rmse[n] <- scores$rmse[n] + rmse(r_pred, r_true) # nolint
+      scores$mae[n] <- scores$mae[n] + mae(r_pred, r_true) # nolint
+      scores$r2[n] <- scores$r2[n] + r2(r_pred, r_true) # nolint
+
+      print(Sys.time() - t1)
+    }
   }
   return(scores / t)
 }

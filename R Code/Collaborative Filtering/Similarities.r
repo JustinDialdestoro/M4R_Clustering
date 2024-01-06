a4880e0c8f0cbc64a08136a0a25a60148065a286
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-07 01:11:10 +0700

    added item based similarity

--- R Code/Collaborative Filtering/Similarities.r
+++ R Code/Collaborative Filtering/Similarities.r
@@ -1,114 +1,256 @@
 library("cluster")
 
 cosine <- function(x, y) {
+  # find non NA entries
   ind <- which(!is.na(x) & !is.na(y))
   if (length(ind) == 0) {
     return(0)
   }
+  # compute cosine similarity
   return((x[ind] %*% y[ind]) /
            (norm(x[ind], type = "2") * norm(y[ind], type = "2")))
 }
 
-gen_cos_sim <- function(ui) {
-  n <- nrow(ui)
-  sim <- matrix(NA, nrow = n, ncol = n)
+gen_cos_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    # construct similarity matrix skeleton
+    n <- nrow(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
 
-  for (i in 1:n) {
-    for (j in i:n) {
-      sim[i, j] <- cosine(ui[i, ], ui[j, ])
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui[i, ], ui[j, ])
+      }
     }
+
+    # fill remainder of matrix
+    sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
+
+  } else {
+    # construct similarity matrix skeleton
+    n <- ncol(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
+
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui[, i], ui[, j])
+      }
+    }
+
+    # fill remainder of matrix
+    sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
   }
-  sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
+
   return(sim)
 }
 
-gen_acos_sim <- function(ui) {
-  n <- nrow(ui)
-  sim <- matrix(NA, nrow = n, ncol = n)
+gen_acos_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    # construct similarity matrix skeleton
+    n <- nrow(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
 
-  mean <- rowMeans(t(ui), na.rm = TRUE)
-  ui0 <- t(t(ui) - mean)
+    # nromalise by column means
+    mean <- rowMeans(t(ui), na.rm = TRUE)
+    ui0 <- t(t(ui) - mean)
 
-  for (i in 1:n) {
-    for (j in i:n) {
-      sim[i, j] <- cosine(ui0[i, ], ui0[j, ])
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui0[i, ], ui0[j, ])
+      }
+    }
+  } else {
+    # construct similarity matrix skeleton
+    n <- ncol(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
+
+    # nromalise by row means
+    mean <- rowMeans(ui, na.rm = TRUE)
+    ui0 <- ui - mean
+
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui0[, i], ui0[, j])
+      }
     }
   }
+
+  # fill remainder of matrix
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
+
+  # transform into similarity matrix
   return((1 + sim) / 2)
 }
 
 gen_pcc_sim <- function(ui) {
-  n <- nrow(ui)
-  sim <- matrix(NA, nrow = n, ncol = n)
+  if (user == TRUE) {
+    # construct similarity matrix skeleton
+    n <- nrow(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
 
-  mean <- rowMeans(ui, na.rm = TRUE)
-  ui0 <- ui - mean
+    # nromalise by row means
+    mean <- rowMeans(ui, na.rm = TRUE)
+    ui0 <- ui - mean
 
-  for (i in 1:n) {
-    for (j in i:n) {
-      sim[i, j] <- cosine(ui0[i, ], ui0[j, ])
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui0[i, ], ui0[j, ])
+      }
+    }
+  } else {
+    # construct similarity matrix skeleton
+    n <- ncol(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
+
+    # nromalise by column means
+    mean <- rowMeans(t(ui), na.rm = TRUE)
+    ui0 <- t(t(ui) - mean)
+
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        sim[i, j] <- cosine(ui0[, i], ui0[, j])
+      }
     }
   }
+
+  # fill remainder of matrix
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
+
+  # transform into similarity matrix
   return((1 + sim) / 2)
 }
 
-gen_jacc_sim <- function(ui) {
-  n <- nrow(ui)
-  sim <- matrix(NA, nrow = n, ncol = n)
+gen_jacc_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    # construct similarity matrix skeleton
+    n <- nrow(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
 
-  for (i in 1:n) {
-    for (j in i:n) {
-      num <- which(!is.na(ui[i, ]) & !is.na(ui[j, ]))
-      denom <- which(!is.na(ui[i, ]) | !is.na(ui[j, ]))
-      sim[i, j] <- length(num) / length(denom)
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        # find non NA entries intersection and union
+        num <- which(!is.na(ui[i, ]) & !is.na(ui[j, ]))
+        denom <- which(!is.na(ui[i, ]) | !is.na(ui[j, ]))
+
+        # compute jaccard index
+        sim[i, j] <- length(num) / length(denom)
+      }
+    }
+  } else {
+    # construct similarity matrix skeleton
+    n <- ncol(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
+
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        # find non NA entries intersection and union
+        num <- which(!is.na(ui[, i]) & !is.na(ui[, j]))
+        denom <- which(!is.na(ui[, i]) | !is.na(ui[, j]))
+
+        # compute jaccard index
+        sim[i, j] <- length(num) / length(denom)
+      }
     }
   }
+
+  # fill remainder of matrix
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
   return(sim)
 }
 
-gen_euc_sim <- function(ui) {
-  sim <- as(dist(ui, "euclidean"), "matrix")
+gen_euc_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    sim <- as(dist(ui, "euclidean"), "matrix")
+  } else {
+    sim <- as(dist(t(ui), "euclidean"), "matrix")
+  }
+
+  # transform into similarity matrix
   return(1 / (1 + sim))
 }
 
-gen_mhat_sim <- function(ui) {
-  sim <- as(dist(ui, "manhattan"), "matrix")
+gen_mhat_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    sim <- as(dist(ui, "manhattan"), "matrix")
+  } else {
+    sim <- as(dist(t(ui), "manhattan"), "matrix")
+  }
+
+  # transform into similarity matrix
   return(1 / (1 + sim))
 }
 
-gen_cheb_sim <- function(ui) {
-  n <- nrow(ui)
-  sim <- matrix(NA, nrow = n, ncol = n)
+gen_cheb_sim <- function(ui, user = TRUE) {
+  if (user == TRUE) {
+    # construct similarity matrix skeleton
+    n <- nrow(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
 
-  for (i in 1:n) {
-    for (j in i:n) {
-      ind <- which(!is.na(ui[i, ]) & !is.na(ui[j, ]))
-      sim[i, j] <- max(abs(ui[i, ][ind] - ui[j, ][ind]))
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        # find non NA entries
+        ind <- which(!is.na(ui[i, ]) & !is.na(ui[j, ]))
+
+        # compute chebyshev distance
+        sim[i, j] <- max(abs(ui[i, ][ind] - ui[j, ][ind]))
+      }
+    }
+  } else {
+    # construct similarity matrix skeleton
+    n <- ncol(ui)
+    sim <- matrix(NA, nrow = n, ncol = n)
+
+    # fill upper triangle of sim matrix
+    for (i in 1:n) {
+      for (j in i:n) {
+        # find non NA entries
+        ind <- which(!is.na(ui[, i]) & !is.na(ui[, j]))
+
+        # compute chebyshev distance
+        sim[i, j] <- max(abs(ui[, i][ind] - ui[, j][ind]))
+      }
     }
   }
+
+  # fill remainder of matrix
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
+
+  # transform into similarity matrix
   return(1 / (1 + sim))
 }
 
 gen_ups_sim <- function(ui) {
+  # construct similarity matrix skeleton
   n <- nrow(ui)
   sim <- matrix(NA, nrow = n, ncol = n)
 
+  # user rating means
   mean <- rowMeans(ui, na.rm = TRUE)
 
+  # fill upper triangle of sim matrix
   for (i in 1:n) {
     for (j in i:n) {
+      # find non NA entries intersection and union
       ind <- which(!is.na(ui[i, ]) & !is.na(ui[j, ]))
       denom <- which(!is.na(ui[i, ]) | !is.na(ui[j, ]))
 
+      # compute UPS
       num <- exp(-mean(abs(ui[i, ][ind] - ui[j, ][ind]))
                  * abs(mean[i] - mean[j])) * length(ind)
       sim[i, j] <- num / length(denom)
     }
   }
+
+  # fill remainder of matrix
   sim[lower.tri(sim, diag = FALSE)] <- t(sim)[lower.tri(t(sim), diag = FALSE)]
   return(sim)
 }

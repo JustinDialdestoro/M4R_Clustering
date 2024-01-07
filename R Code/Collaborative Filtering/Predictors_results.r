03bbedc71b860b1dc9e1101a31d8ff78f305de18
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-07 23:57:59 +0700

    item based predictor comparison plots

--- R Code/Collaborative Filtering/Predictors_results.r
+++ R Code/Collaborative Filtering/Predictors_results.r
@@ -16,32 +16,116 @@ source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 # set range of k values to test over
 krange <- seq(from = 10, to = 100, by = 10)
 
-# evaluate cosine, adjusted cosine, and pcc
-euc_scores_1 <- cval(u100k, 10, gen_euc_sim, weighted_sum, krange)
-euc_scores_2 <- cval(u100k, 10, gen_euc_sim, mean_centered, krange)
-euc_scores_3 <- cval(u100k, 10, gen_euc_sim, z_score, krange)
-euc_scores_4 <- cval(u100k, 10, gen_euc_sim, discrete, krange)
+# # evaluate cosine, adjusted cosine, and pcc
+# euc_scores_wsum_u <- cval(u100k, 10, krange,  gen_euc_sim, weighted_sum)
+# euc_scores_mcent_u <- cval(u100k, 10, krange,  gen_euc_sim, mean_centered)
+# euc_scores_z_u <- cval(u100k, 10, krange,  gen_euc_sim, z_score)
+# euc_scores_disc_u <- cval(u100k, 10, krange,  gen_euc_sim, discrete)
 
-plot(krange, euc_scores_1$rmse, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.95, 1.105))
-lines(krange, euc_scores_2$rmse, type = "l", col = viridis(4)[2], lwd = 2)
-lines(krange, euc_scores_3$rmse, type = "l", col = viridis(4)[3], lwd = 2)
-lines(krange, euc_scores_4$rmse, type = "l", col = viridis(4)[4], lwd = 2)
-legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = viridis(4), lwd = 2, cex = 1)
+# scores <- rbind(euc_scores_wsum_u, euc_scores_mcent_u,
+#                 euc_scores_z_u, euc_scores_disc_u)
 
-plot(krange, euc_scores_1$mae, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.74, 0.8))
-lines(krange, euc_scores_2$mae, type = "l", col = viridis(4)[2], lwd = 2)
-lines(krange, euc_scores_3$mae, type = "l", col = viridis(4)[3], lwd = 2)
-lines(krange, euc_scores_4$mae, type = "l", col = viridis(4)[4], lwd = 2)
-legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = viridis(4), lwd = 2, cex = 1)
+# ymax <- max(scores$rmse)
+# ymin <- min(scores$rmse)
+# ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_1$r2, type = "l", col = viridis(4)[1], lwd = 2,
-     ylim = c(0.15, 0.29))
-lines(krange, euc_scores_2$r2, type = "l", col = viridis(4)[2], lwd = 2)
-lines(krange, euc_scores_3$r2, type = "l", col = viridis(4)[3], lwd = 2)
-lines(krange, euc_scores_4$r2, type = "l", col = viridis(4)[4], lwd = 2)
+# plot(krange, euc_scores_wsum_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+#      col = viridis(4)[1], xlab = "k neighbours", ylab = "RMSE",
+#      ylim = c(ymin - ygap, ymax + ygap))
+# lines(krange, euc_scores_mcent_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[2])
+# lines(krange, euc_scores_z_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(5)[3])
+# lines(krange, euc_scores_disc_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[4])
+# legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
+#        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)
+
+# ymax <- max(scores$mae)
+# ymin <- min(scores$mae)
+# ygap <- 0.2 * (ymax - ymin)
+
+# plot(krange, euc_scores_wsum_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+#      col = viridis(4)[1], xlab = "k neighbours", ylab = "MAE",
+#      ylim = c(ymin - ygap, ymax + ygap))
+# lines(krange, euc_scores_mcent_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[2])
+# lines(krange, euc_scores_z_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[3])
+# lines(krange, euc_scores_disc_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[4])
+# legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
+#        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)
+
+# ymax <- max(scores$r2)
+# ymin <- min(scores$r2)
+# ygap <- 0.2 * (ymax - ymin)
+
+# plot(krange, euc_scores_wsum_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+#      col = viridis(4)[1], xlab = "k neighbours", ylab = "R2",
+#      ylim = c(ymin - ygap, ymax + ygap))
+# lines(krange, euc_scores_mcent_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[2])
+# lines(krange, euc_scores_z_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[3])
+# lines(krange, euc_scores_disc_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+#       col = viridis(4)[4])
+# legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
+#        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)
+
+# # evaluate cosine, adjusted cosine, and pcc
+# euc_scores_wsum_i <- cval(u100k, 10, krange, gen_euc_sim, weighted_sum, FALSE)
+# euc_scores_mean_i <- cval(u100k, 10, krange,  gen_euc_sim, mean_centered, FALSE)
+# euc_scores_z_i <- cval(u100k, 10, krange,  gen_euc_sim, z_score, FALSE)
+# euc_scores_disc_i <- cval(u100k, 10, krange,  gen_euc_sim, discrete, FALSE)
+
+scores <- rbind(euc_scores_wsum_i, euc_scores_mean_i,
+                euc_scores_z_i, euc_scores_disc_i)
+
+ymax <- max(scores$rmse)
+ymin <- min(scores$rmse)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, euc_scores_wsum_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(4)[1], xlab = "k neighbours", ylab = "RMSE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, euc_scores_mean_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[2])
+lines(krange, euc_scores_z_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[3])
+lines(krange, euc_scores_disc_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[4])
 legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = viridis(4), lwd = 2, cex = 1)
+       col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)
+
+ymax <- max(scores$mae)
+ymin <- min(scores$mae)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, euc_scores_wsum_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(4)[1], xlab = "k neighbours", ylab = "MAE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, euc_scores_mean_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[2])
+lines(krange, euc_scores_z_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[3])
+lines(krange, euc_scores_disc_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[4])
+legend("topright", c("weighted sum", "mean centered", "z score", "discrete"),
+       col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)
+
+ymax <- max(scores$r2)
+ymin <- min(scores$r2)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, euc_scores_wsum_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(4)[1], xlab = "k neighbours", ylab = "R2",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, euc_scores_mean_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[2])
+lines(krange, euc_scores_z_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[3])
+lines(krange, euc_scores_disc_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[4])
+legend("right", c("weighted sum", "mean centered", "z score", "discrete"),
+       col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8)

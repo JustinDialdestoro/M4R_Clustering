d49e315c037bbb86dbceb2b274480ab76409cb41
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-01-27 21:38:19 +0000

    write new item feature file

--- R Code/Data_preparation.r
+++ R Code/Data_preparation.r
@@ -63,7 +63,8 @@ u100k_dem <- read.table("Data/ml-100k/ml-100k/u.user", sep = "|",
 u100k_dem$userID <- NULL
 
 # write cleaned 100k user demographic data into file
-write.csv(u100k_dem, file = "M4R_Clustering/Data/u100k_dem.csv", row.names = FALSE)
+write.csv(u100k_dem, file = "M4R_Clustering/Data/u100k_dem.csv",
+          row.names = FALSE)
 
 # clean ml 100k item feature data ----------------------------------------------
 

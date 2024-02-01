aead56bc8bc288a98edf28199aba362d14641a5e
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-01 09:58:11 +0000

    predictor comparison plots

--- R Code/Collaborative Filtering/Predictors_results.r
+++ R Code/Collaborative Filtering/Predictors_results.r
@@ -8,118 +8,156 @@ source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
 source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 
 # set range of k values to test over
-krange <- seq(from = 10, to = 100, by = 10)
+krange <- seq(from = 10, to = 300, by = 10)
 
 # # evaluate cosine, adjusted cosine, and pcc
-euc_scores_wsum_u <- cval(ml100k, 10, krange,  gen_euc_sim, weighted_sum)
-euc_scores_mcent_u <- cval(ml100k, 10, krange,  gen_euc_sim, mean_centered)
-euc_scores_z_u <- cval(ml100k, 10, krange,  gen_euc_sim, z_score)
-euc_scores_disc_u <- cval(ml100k, 10, krange,  gen_euc_sim, discrete)
+wsum_u <- cval(ml100k, 10, krange,  gen_acos_sim, weighted_sum)
+mcent_u <- cval(ml100k, 10, krange,  gen_acos_sim, mean_centered)
+zscore_u <- cval(ml100k, 10, krange,  gen_acos_sim, z_score)
+disc_u <- cval(ml100k, 10, krange,  gen_acos_sim, discrete)
 
-scores <- rbind(euc_scores_wsum_u, euc_scores_mcent_u,
-                euc_scores_z_u, euc_scores_disc_u)
+scores_u <- rbind(wsum_u, mcent_u, zscore_u, disc_u)
 
-ymax <- max(scores$rmse)
-ymin <- min(scores$rmse)
+ymax <- max(scores_u$rmse)
+ymin <- min(scores_u$rmse)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mcent_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(5)[3])
-lines(krange, euc_scores_disc_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
 
-ymax <- max(scores$mae)
-ymin <- min(scores$mae)
+ymax <- max(scores_u$mae)
+ymin <- min(scores_u$mae)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "MAE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mcent_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[3])
-lines(krange, euc_scores_disc_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
 
-ymax <- max(scores$r2)
-ymin <- min(scores$r2)
+ymax <- max(scores_u$r2)
+ymin <- min(scores_u$r2)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "R2",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mcent_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[3])
-lines(krange, euc_scores_disc_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
+
+ymax <- max(scores_u$online)
+ymin <- min(scores_u$online)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(4)[1], xlab = "k neighbours",
+     ylab = "Online phase time (seconds)", ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mcent_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[2])
+lines(krange, zscore_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[3])
+lines(krange, disc_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
+       col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)
+
+# write user predictor results into file
+write.csv(scores_u, file = "M4R_Clustering/Results/pred_u.csv",
+          row.names = FALSE)
 
 # evaluate cosine, adjusted cosine, and pcc
-euc_scores_wsum_i <- cval(ml100k, 10, krange, gen_euc_sim, weighted_sum, FALSE)
-euc_scores_mean_i <- cval(ml100k, 10, krange,  gen_euc_sim, mean_centered, FALSE)
-euc_scores_z_i <- cval(ml100k, 10, krange,  gen_euc_sim, z_score, FALSE)
-euc_scores_disc_i <- cval(ml100k, 10, krange,  gen_euc_sim, discrete, FALSE)
+wsum_i <- cval(ml100k, 10, krange, gen_acos_sim, weighted_sum, FALSE)
+mean_i <- cval(ml100k, 10, krange,  gen_acos_sim, mean_centered, FALSE)
+zscore_i <- cval(ml100k, 10, krange,  gen_acos_sim, z_score, FALSE)
+disc_i <- cval(ml100k, 10, krange,  gen_acos_sim, discrete, FALSE)
 
-scores <- rbind(euc_scores_wsum_i, euc_scores_mean_i,
-                euc_scores_z_i, euc_scores_disc_i)
+scores_i <- rbind(wsum_i, mean_i, zscore_i, disc_i)
 
-ymax <- max(scores$rmse)
-ymin <- min(scores$rmse)
+ymax <- max(scores_i$rmse)
+ymin <- min(scores_i$rmse)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mean_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mean_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[3])
-lines(krange, euc_scores_disc_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_i$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
 
-ymax <- max(scores$mae)
-ymin <- min(scores$mae)
+ymax <- max(scores_i$mae)
+ymin <- min(scores_i$mae)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "MAE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mean_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mean_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[3])
-lines(krange, euc_scores_disc_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_i$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
 
-ymax <- max(scores$r2)
-ymin <- min(scores$r2)
+ymax <- max(scores_i$r2)
+ymin <- min(scores_i$r2)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, euc_scores_wsum_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
      col = viridis(4)[1], xlab = "k neighbours", ylab = "R2",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, euc_scores_mean_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mean_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[2])
-lines(krange, euc_scores_z_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[3])
-lines(krange, euc_scores_disc_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_i$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = viridis(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
+
+ymax <- max(scores_i$online)
+ymin <- min(scores_i$online)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_i$online, lty = 2, type = "b", pch = 4, lwd = 2,
+     col = viridis(4)[1], xlab = "k neighbours",
+     ylab = "Online phase time (seconds)", ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mean_i$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[2])
+lines(krange, zscore_i$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[3])
+lines(krange, disc_i$online, lty = 2, type = "b", pch = 4, lwd = 2,
+      col = viridis(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
+       col = viridis(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)
+
+# write item predictor results into file
+write.csv(scores_i, file = "M4R_Clustering/Results/pred_i.csv",
+          row.names = FALSE)

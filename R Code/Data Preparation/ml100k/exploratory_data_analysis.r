07df900a3c35f28e6f65996345ae6215bf286cea
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-06 18:26:11 +0000

    exploratory data analysis file

---
+++ R Code/Data Preparation/ml100k/exploratory_data_analysis.r
@@ -0,0 +1,10 @@
+# read in the data
+ml100k <- read.csv("M4R_Clustering/Data/ml100k.csv")
+
+# load CF functions
+source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
+
+ui <- gen_ui_matrix(ml100k, ml100k)
+
+# average user ratings
+u_mean <- rowMeans(ui, na.rm = TRUE)

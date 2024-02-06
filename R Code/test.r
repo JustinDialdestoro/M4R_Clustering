8b658d03c7f7cc2f7ff4093484cce41bc12cc658
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-06 13:36:30 +0000

    changed variable names

--- R Code/test.r
+++ R Code/test.r
@@ -2,16 +2,15 @@ source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
 source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
 source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 
-u100k <- read.table("M4R_Clustering/Data/u.data",
-                    col.names = c("userID", "filmID", "rating", "timestamp"))
+ml100k <- read.csv("M4R_Clustering/Data/ml100k.csv")
 
 t1 <- Sys.time()
-f_ind <- t_fold_index(u100k, 10)
-f <- t_fold(u100k, f_ind)
+f_ind <- t_fold_index(ml100k, 10)
+f <- t_fold(ml100k, f_ind)
 t2 <- Sys.time()
 
 t1 <- Sys.time()
-ui <- gen_ui_matrix(u100k, f[[1]])
+ui <- gen_ui_matrix(ml100k, f[[1]])
 t2 <- Sys.time()
 
 # t1 <- Sys.time()

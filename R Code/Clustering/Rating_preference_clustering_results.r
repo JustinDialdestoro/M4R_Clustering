8bf61c5be2890589fdce5d646f728e529c8cb21f
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-06 18:16:21 +0000

    user preference clustering comparison

--- R Code/Clustering/Rating_preference_clustering_results.r
+++ R Code/Clustering/Rating_preference_clustering_results.r
@@ -33,66 +33,66 @@ pref_clust_u <- cbind(predictor = c(rep("weighted sum", n),
 write.csv(pref_clust_u, file = "M4R_Clustering/Results/pref_clust_u.csv",
           row.names = FALSE)
 
-ymax <- max(scores$rmse)
-ymin <- min(scores$rmse)
+ymax <- max(pref_clust_u$rmse)
+ymin <- min(pref_clust_u$rmse)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$rmse, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
+       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)
 
-ymax <- max(scores$mae)
-ymin <- min(scores$mae)
+ymax <- max(pref_clust_u$mae)
+ymin <- min(pref_clust_u$mae)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc$mae, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$mae, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
+       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)
 
-ymax <- max(scores$r2)
-ymin <- min(scores$r2)
+ymax <- max(pref_clust_u$r2)
+ymin <- min(pref_clust_u$r2)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc$r2, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$r2, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
-       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 1, horiz = TRUE)
+       col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)
 
-ymax <- max(scores$online)
-ymin <- min(scores$online)
+ymax <- max(pref_clust_u$online)
+ymin <- min(pref_clust_u$online)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum$online, lty = 2, type = "b", pch = 4, lwd = 2,
+plot(krange, wsum_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours",
      ylab = "Online phase time (seconds)", ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent$online, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, mcent_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore$online, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, zscore_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc$online, lty = 2, type = "b", pch = 4, lwd = 2,
+lines(krange, disc_u$online, lty = 2, type = "b", pch = 4, lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete"),
        col = hue_pal()(4), lty = 2, pch = 4, lwd = 2, cex = 0.8, horiz = TRUE)

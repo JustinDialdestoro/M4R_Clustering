2bb06da64a34fd18322dd84ce58d087407008dd3
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-08 22:20:07 +0000

    ups metric comparison

--- R Code/Clustering/Rating_preference_clustering_results.r
+++ R Code/Clustering/Rating_preference_clustering_results.r
@@ -13,55 +13,55 @@ source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
 krange <- seq(from = 10, to = 300, by = 10)
 n <- length(krange)
 
-wsum_clust <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, weighted_sum,
-                              acos_clust, 3, 4)
-mcent_clust <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, mean_centered,
+wsum_acos_c <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, weighted_sum,
                                acos_clust, 3, 4)
-zscore_clust <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, z_score,
+mcent_acos_c <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, mean_centered,
                                 acos_clust, 3, 4)
-disc_clust <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, discrete,
-                              acos_clust, 3, 4)
+zscore_acos_c <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, z_score,
+                                 acos_clust, 3, 4)
+disc_acos_c <- cval_pref_clust(ml100k, 10, krange, gen_acos_sim, discrete,
+                               acos_clust, 3, 4)
 
-pref_clust_u <- rbind(wsum_clust, mcent_clust, zscore_clust, disc_clust)
+pref_clust_acos <- rbind(wsum_acos_c, mcent_acos_c, zscore_acos_c, disc_acos_c)
 
-pref_clust_u <- cbind(predictor = c(rep("weighted sum", n),
-                                    rep("mean centred", n),
-                                    rep("z score", n),
-                                    rep("discrete", n)), pref_clust_u)
+pref_clust_acos <- cbind(predictor = c(rep("weighted sum", n),
+                                       rep("mean centred", n),
+                                       rep("z score", n),
+                                       rep("discrete", n)), pref_clust_acos)
 
 # write user preference clustering results into file
-write.csv(pref_clust_u, file = "M4R_Clustering/Results/pref_clust_u.csv",
+write.csv(pref_clust_acos, file = "M4R_Clustering/Results/pref_clust_acos.csv",
           row.names = FALSE)
 
 # read previously obtained prediction comparison
-pred_u <- read.csv("M4R_Clustering/Results/pred_u.csv")
-wsum_pred <- pred_u[pred_u$predictor == "weighted sum", 2:6]
-mcent_pred <- pred_u[pred_u$predictor == "mean centred", 2:6]
-zscore_pred <- pred_u[pred_u$predictor == "z score", 2:6]
-disc_pred <- pred_u[pred_u$predictor == "discrete", 2:6]
+pred_acos <- read.csv("M4R_Clustering/Results/pred_u.csv")
+wsum_acos <- pred_u[pred_u$predictor == "weighted sum", 2:6]
+mcent_acos <- pred_u[pred_u$predictor == "mean centred", 2:6]
+zscore_acos <- pred_u[pred_u$predictor == "z score", 2:6]
+disc_acos <- pred_u[pred_u$predictor == "discrete", 2:6]
 
-full <- rbind(pref_clust_u, pred_u)
+full <- rbind(pref_clust_acos, pred_acos)
 
 ymax <- max(full$rmse)
 ymin <- min(full$rmse)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum_clust$rmse, lty = 1, type = "l", lwd = 2,
+plot(krange, wsum_acos_c$rmse, lty = 1, type = "l", lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "RMSE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent_clust$rmse, lty = 1, type = "l", lwd = 2,
+lines(krange, mcent_acos_c$rmse, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_clust$rmse, lty = 1, type = "l", lwd = 2,
+lines(krange, zscore_acos_c$rmse, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_clust$rmse, lty = 1, type = "l", lwd = 2,
+lines(krange, disc_acos_c$rmse, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
-lines(krange, wsum_pred$rmse, lty = 2, type = "l", lwd = 2,
+lines(krange, wsum_acos$rmse, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[1])
-lines(krange, mcent_pred$rmse, lty = 2, type = "l", lwd = 2,
+lines(krange, mcent_acos$rmse, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_pred$rmse, lty = 2, type = "l", lwd = 2,
+lines(krange, zscore_acos$rmse, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_pred$rmse, lty = 2, type = "l", lwd = 2,
+lines(krange, disc_acos$rmse, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
                    "clustered"),
@@ -72,22 +72,22 @@ ymax <- max(full$mae)
 ymin <- min(full$mae)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum_clust$mae, lty = 1, type = "l", lwd = 2,
+plot(krange, wsum_acos_c$mae, lty = 1, type = "l", lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "MAE",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent_clust$mae, lty = 1, type = "l", lwd = 2,
+lines(krange, mcent_acos_c$mae, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_clust$mae, lty = 1, type = "l", lwd = 2,
+lines(krange, zscore_acos_c$mae, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_clust$mae, lty = 1, type = "l", lwd = 2,
+lines(krange, disc_acos_c$mae, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
-lines(krange, wsum_pred$mae, lty = 2, type = "l", lwd = 2,
+lines(krange, wsum_acos$mae, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[1])
-lines(krange, mcent_pred$mae, lty = 2, type = "l", lwd = 2,
+lines(krange, mcent_acos$mae, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_pred$mae, lty = 2, type = "l", lwd = 2,
+lines(krange, zscore_acos$mae, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_pred$mae, lty = 2, type = "l", lwd = 2,
+lines(krange, disc_acos$mae, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
                    "clustered"),
@@ -98,22 +98,22 @@ ymax <- max(full$r2)
 ymin <- min(full$r2)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum_clust$r2, lty = 1, type = "l", lwd = 2,
+plot(krange, wsum_acos_c$r2, lty = 1, type = "l", lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "R2",
      ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent_clust$r2, lty = 1, type = "l", lwd = 2,
+lines(krange, mcent_acos_c$r2, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_clust$r2, lty = 1, type = "l", lwd = 2,
+lines(krange, zscore_acos_c$r2, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_clust$r2, lty = 1, type = "l", lwd = 2,
+lines(krange, disc_acos_c$r2, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
-lines(krange, wsum_pred$r2, lty = 2, type = "l", lwd = 2,
+lines(krange, wsum_acos$r2, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[1])
-lines(krange, mcent_pred$r2, lty = 2, type = "l", lwd = 2,
+lines(krange, mcent_acos$r2, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_pred$r2, lty = 2, type = "l", lwd = 2,
+lines(krange, zscore_acos$r2, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_pred$r2, lty = 2, type = "l", lwd = 2,
+lines(krange, disc_acos$r2, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
                    "clustered"),
@@ -124,22 +124,155 @@ ymax <- max(full$online)
 ymin <- min(full$online)
 ygap <- 0.2 * (ymax - ymin)
 
-plot(krange, wsum_clust$online, lty = 1, type = "l", lwd = 2,
+plot(krange, wsum_acos_c$online, lty = 1, type = "l", lwd = 2,
      col = hue_pal()(4)[1], xlab = "k neighbours",
      ylab = "Online phase time (seconds)", ylim = c(ymin - ygap, ymax + ygap))
-lines(krange, mcent_clust$online, lty = 1, type = "l", lwd = 2,
+lines(krange, mcent_acos_c$online, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_clust$online, lty = 1, type = "l", lwd = 2,
+lines(krange, zscore_acos_c$online, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_clust$online, lty = 1, type = "l", lwd = 2,
+lines(krange, disc_acos_c$online, lty = 1, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
-lines(krange, wsum_pred$online, lty = 2, type = "l", lwd = 2,
+lines(krange, wsum_acos$online, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[1])
-lines(krange, mcent_pred$online, lty = 2, type = "l", lwd = 2,
+lines(krange, mcent_acos$online, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[2])
-lines(krange, zscore_pred$online, lty = 2, type = "l", lwd = 2,
+lines(krange, zscore_acos$online, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[3])
-lines(krange, disc_pred$online, lty = 2, type = "l", lwd = 2,
+lines(krange, disc_acos$online, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
+                   "clustered"),
+       col = c(hue_pal()(4), "black"),
+       lty = c(2, 2, 2, 2, 1), lwd = 2, cex = 0.8, horiz = TRUE)
+
+wsum_ups_c <- cval_pref_clust(ml100k, 10, krange, gen_ups_sim, weighted_sum,
+                              ups_clust, 3, 4)
+mcent_ups_c <- cval_pref_clust(ml100k, 10, krange, gen_ups_sim, mean_centered,
+                               ups_clust, 3, 4)
+zscore_ups_c <- cval_pref_clust(ml100k, 10, krange, gen_ups_sim, z_score,
+                                ups_clust, 3, 4)
+disc_ups_c <- cval_pref_clust(ml100k, 10, krange, gen_ups_sim, discrete,
+                              ups_clust, 3, 4)
+
+pref_clust_ups <- rbind(wsum_ups_c, mcent_ups_c, zscore_ups_c, disc_ups_c)
+
+pref_clust_ups <- cbind(predictor = c(rep("weighted sum", n),
+                                      rep("mean centred", n),
+                                      rep("z score", n),
+                                      rep("discrete", n)), pref_clust_ups)
+
+# write user preference clustering results into file
+write.csv(pref_clust_ups, file = "M4R_Clustering/Results/pref_clust_ups.csv",
+          row.names = FALSE)
+
+# read previously obtained prediction comparison
+pred_ups <- read.csv("M4R_Clustering/Results/pred_u.csv")
+wsum_ups <- pred_u[pred_u$predictor == "weighted sum", 2:6]
+mcent_ups <- pred_u[pred_u$predictor == "mean centred", 2:6]
+zscore_ups <- pred_u[pred_u$predictor == "z score", 2:6]
+disc_ups <- pred_u[pred_u$predictor == "discrete", 2:6]
+
+full <- rbind(pref_clust_ups, pred_ups)
+
+ymax <- max(full$rmse)
+ymin <- min(full$rmse)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_ups_c$rmse, lty = 1, type = "l", lwd = 2,
+     col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "RMSE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mcent_ups_c$rmse, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups_c$rmse, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups_c$rmse, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+lines(krange, wsum_ups$rmse, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[1])
+lines(krange, mcent_ups$rmse, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups$rmse, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups$rmse, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
+                   "clustered"),
+       col = c(hue_pal()(4), "black"),
+       lty = c(2, 2, 2, 2, 1), lwd = 2, cex = 0.8, horiz = TRUE)
+
+ymax <- max(full$mae)
+ymin <- min(full$mae)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_ups_c$mae, lty = 1, type = "l", lwd = 2,
+     col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "MAE",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mcent_ups_c$mae, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups_c$mae, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups_c$mae, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+lines(krange, wsum_ups$mae, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[1])
+lines(krange, mcent_ups$mae, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups$mae, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups$mae, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
+                   "clustered"),
+       col = c(hue_pal()(4), "black"),
+       lty = c(2, 2, 2, 2, 1), lwd = 2, cex = 0.8, horiz = TRUE)
+
+ymax <- max(full$r2)
+ymin <- min(full$r2)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_ups_c$r2, lty = 1, type = "l", lwd = 2,
+     col = hue_pal()(4)[1], xlab = "k neighbours", ylab = "R2",
+     ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mcent_ups_c$r2, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups_c$r2, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups_c$r2, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+lines(krange, wsum_ups$r2, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[1])
+lines(krange, mcent_ups$r2, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups$r2, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups$r2, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
+                   "clustered"),
+       col = c(hue_pal()(4), "black"),
+       lty = c(2, 2, 2, 2, 1), lwd = 2, cex = 0.8, horiz = TRUE)
+
+ymax <- max(full$online)
+ymin <- min(full$online)
+ygap <- 0.2 * (ymax - ymin)
+
+plot(krange, wsum_ups_c$online, lty = 1, type = "l", lwd = 2,
+     col = hue_pal()(4)[1], xlab = "k neighbours",
+     ylab = "Online phase time (seconds)", ylim = c(ymin - ygap, ymax + ygap))
+lines(krange, mcent_ups_c$online, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups_c$online, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups_c$online, lty = 1, type = "l", lwd = 2,
+      col = hue_pal()(4)[4])
+lines(krange, wsum_ups$online, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[1])
+lines(krange, mcent_ups$online, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[2])
+lines(krange, zscore_ups$online, lty = 2, type = "l", lwd = 2,
+      col = hue_pal()(4)[3])
+lines(krange, disc_ups$online, lty = 2, type = "l", lwd = 2,
       col = hue_pal()(4)[4])
 legend("bottom", c("weighted sum", "mean centered", "z score", "discrete",
                    "clustered"),

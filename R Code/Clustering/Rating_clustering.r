da11dddef2a5e1cf43b0063a70c62e9004d9a230
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-02-13 00:47:10 +0000

    modified cluster cval to search over a k range

--- R Code/Clustering/Rating_clustering.r
+++ R Code/Clustering/Rating_clustering.r
@@ -3,22 +3,22 @@ source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
 source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
 source("M4R_Clustering/R Code/Clustering/Rating_preference_clustering.r")
 
-rating_clust <- function(ui, k, user = TRUE) {
+rating_clust <- function(ui, n, metric, user = TRUE) {
   # create similarity matrix
-  sim <- gen_euc_sim(ui, user) # nolint
+  sim <- metric(ui, user) # nolint
   sim[is.na(sim)] <- 0
 
   # k-means clustering
-  cluster <- kmeans(sim, k)
+  cluster <- kmeans(sim, n)
 
   return(cluster$cluster)
 }
 
-cval_rating_clust <- function(df, t, k, n_range,
-                              metric, pred_func, user = TRUE) {
-  m <- length(n_range)
+cval_clust <- function(df, t, n, k_range, metric, pred_func, user = TRUE) {
+  n <- length(k_range)
   # initial scores table
-  scores <- data.frame(rmse = rep(0, m), mae = rep(0, m), r2 = rep(0, m))
+  scores <- data.frame(rmse = rep(0, n), mae = rep(0, n), r2 = rep(0, n),
+                       offline = rep(0, t), online = rep(0, n))
 
   # t-fold creation
   cval_f_i <- t_fold_index(df, t) # nolint
@@ -26,54 +26,58 @@ cval_rating_clust <- function(df, t, k, n_range,
 
   # loop over each fold
   for (i in 1:t) {
+    print(paste("Offline phase for fold", i, ":"))
+    t1 <- Sys.time()
 
-    # loop over every n
-    for (n in seq_along(n_range)) {
-      print(paste("Offline phase for n = ", n_range[n], ", fold", i, ":"))
-      t1 <- Sys.time()
+    # ui and similarity matrix
+    ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
 
-      # ui and similarity matrix
-      ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
+    # create user clusters
+    clusters <- rating_clust(ui, n, metric, user)
 
-      # create rating clusters
-      clusters <- rating_clust(ui, n_range[n], user)
+    # segment user ratings matrix into the n clusters
+    uis <- replicate(n, c())
 
-      # segment user ratings matrix into the n clusters
-      uis <- replicate(n_range[n], c())
-
-      if (user == TRUE) {
-        for (j in 1:n_range[n]) {
-          uis[[j]] <- ui[which(clusters == j), ]
-        }
-      } else {
-        for (j in 1:n_range[n]) {
-          uis[[j]] <- ui[, which(clusters == j)]
-        }
+    if (user == TRUE) {
+      for (j in 1:n) {
+        uis[[j]] <- ui[which(clusters == j), ]
       }
-
-      # similarity matrix for each segmented ui matrix
-      sims <- replicate(n_range[n], c())
-      for (j in 1:n_range[n]) {
-        sims[[j]] <- metric(uis[[j]], user)
+    } else {
+      for (j in 1:n) {
+        uis[[j]] <- ui[, which(clusters == j)]
       }
+    }
 
-      print(Sys.time() - t1)
+    # similarity matrix for each segmented ui matrix
+    sims <- replicate(n, c())
+    for (j in 1:n) {
+      sims[[j]] <- metric(uis[[j]], user)
+    }
 
-      print("Online phase:")
+    time <- Sys.time() - t1
+    print(time)
+    scores$offline[i] <- time
+
+    # loop over every k
+    for (k in seq_along(k_range)) {
+      print(paste("Online phase for k =", k_range[k]))
       t1 <- Sys.time()
 
       # predict on test fold ratings
       r_pred <- pred_fold_clust(df, cval_f_i[[i]], uis, sims, pred_func, # nolint
-                                k, clusters, user)
+                                k_range[k], clusters, user)
       r_true <- df$rating[cval_f_i[[i]]]
 
       # error metrics
-      scores$rmse[n] <- scores$rmse[n] + rmse(r_pred, r_true) # nolint
-      scores$mae[n] <- scores$mae[n] + mae(r_pred, r_true) # nolint
-      scores$r2[n] <- scores$r2[n] + r2(r_pred, r_true) # nolint
+      scores$rmse[k] <- scores$rmse[k] + rmse(r_pred, r_true) # nolint
+      scores$mae[k] <- scores$mae[k] + mae(r_pred, r_true) # nolint
+      scores$r2[k] <- scores$r2[k] + r2(r_pred, r_true) # nolint
 
-      print(Sys.time() - t1)
+      time <- Sys.time() - t1
+      print(time)
+      scores$online[k] <- scores$online[k] + time
     }
   }
-  return(scores / t)
+  scores[c(1:3, 5)] <- scores[c(1:3, 5)] / t
+  return(scores)
 }

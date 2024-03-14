6a361a25b6ef6af9634728ba77eb58ef48becd07
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-03-14 13:29:36 +0000

    fuzzy famd function

--- R Code/Fuzzy Clustering/Mixed_fuzzy_clustering.r
+++ R Code/Fuzzy Clustering/Mixed_fuzzy_clustering.r
@@ -55,6 +55,7 @@ fuzzy_gow <- function(df, c, m, user = TRUE, e = 1e-5) {
   }
   # compute loss
   loss <- u_new**m %*% d[, ind]**2
+
   return(list(u = u_new, centroids = v_new, loss = sum(diag(loss))))
 }
 
@@ -264,6 +265,7 @@ fuzzy_mixed_k <- function(df, c, m, user = TRUE, e = 1e-5) {
   }
   # compute loss
   loss <- u_new**m %*% d[, ind]**2
+
   return(list(u = u_new, centroids = v_new, loss = sum(diag(loss))))
 }
 
@@ -377,4 +379,40 @@ fuzzy_mskmeans <- function(df, c, m, user = TRUE, e = 1e-5,
   }
 
   return(out)
+}
+
+fuzzy_famd <- function(df, c, m, user = TRUE, e = 1e-5, p = 3) {
+  # initialise number of data points
+  n <- nrow(df)
+
+  # transform data into lower dimensional space
+  x <- FAMD(famd_df(df, user), p, graph = FALSE)$ind$coord # nolint
+
+  # initialise cluster memberships u^(0)
+  u_old <- matrix(runif(n * c), nrow = c)
+  u_old <- t(t(u_old) / colSums(u_old, na.rm = TRUE))
+
+  # initialise centroids v^(1)
+  v_new <- (u_old**m %*% x) / rowSums(u_old**m, na.rm = TRUE)
+
+  # update u^(1)
+  d <- euc_dsim(x, v_new, c)**(2 / (m - 1)) # nolint
+  u_new <- 1 / t(t(d) * colSums(1 / d, na.rm = TRUE))
+
+  # iterate until convergence
+  while (norm(u_old - u_new, type = "F") > e) {
+    u_old <- u_new
+
+    # update v^(l+1)
+    v_new <- (u_new**m %*% x) / rowSums(u_new**m, na.rm = TRUE)
+
+    # update u^(l+1)
+    d <- euc_dsim(x, v_new, c)**(2 / (m - 1)) # nolint
+    u_new <- 1 / t(t(d) * colSums(1 / d, na.rm = TRUE))
+  }
+
+  # compute loss
+  loss <- sum(diag(u_new**m %*% t(d**((m - 1) / 2)))) # nolint
+
+  return(list(u = u_new, centroids = v_new, loss = sum(loss)))
 }

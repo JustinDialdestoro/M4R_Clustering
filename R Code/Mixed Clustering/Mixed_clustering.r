5cadb5864c17ed22658ee409726ad216c45e660e
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-05-25 20:45:18 +0100

    mixed clustering using best k clusters results

--- R Code/Mixed Clustering/Mixed_clustering.r
+++ R Code/Mixed Clustering/Mixed_clustering.r
@@ -1,5 +1,5 @@
 source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
-source("M4R_Clustering/R Code/Clustering/Rating_preference_clustering.r")
+source("M4R_Clustering/R Code/Clustering/Rating_clustering.r")
 source("M4R_Clustering/R Code/Mixed Clustering/Mixed_clustering_functions.r")
 
 best_n <- function(df, n_range, clust_func, user = TRUE, p = FALSE) {
@@ -42,7 +42,7 @@ cval_mixed_clust <- function(df, df_feat, t, n, k_range, metric, pred_func,
     ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
 
     # create user clusters
-    clusters <- clust_func(df_feat, n, user)
+    clusters <- clust_func(df_feat, n, user)$clusters
 
     # segment user ratings matrix into the n clusters
     uis <- replicate(n, c())
@@ -88,5 +88,80 @@ cval_mixed_clust <- function(df, df_feat, t, n, k_range, metric, pred_func,
     }
   }
   scores[c(1:3, 5)] <- scores[c(1:3, 5)] / t
+  return(scores)
+}
+
+cval_mixed_clust_pred <- function(df, df_feat, t, n_range, metric, pred_func,
+                                  clust_func, sim = TRUE, user = TRUE) {
+  nn <- length(n_range)
+  # initial scores table
+  scores <- data.frame(rmse = rep(0, nn), mae = rep(0, nn), r2 = rep(0, nn),
+                       offline = rep(0, nn), online = rep(0, nn))
+
+  # t-fold creation
+  cval_f_i <- t_fold_index(df, t, user) # nolint
+  cval_f <- t_fold(df, cval_f_i) # nolint
+
+  # loop over each fold
+  for (n in 1:length(n_range)) {
+    print(paste("Testing with", n_range[n], "clusters:"))
+
+    for (i in 1:t) {
+      print(paste("Offline phase for fold", i, ":"))
+      t1 <- Sys.time()
+
+      # ui and similarity matrix
+      ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
+
+      # create user clusters
+      clusters <- clust_func(df_feat, n_range[n], user)$clusters
+
+      # segment user ratings matrix into the n clusters
+      uis <- replicate(n, c())
+
+      if (user == TRUE) {
+        for (j in 1:n_range[n]) {
+          uis[[j]] <- ui[which(clusters == j), ]
+        }
+      } else {
+        for (j in 1:n_range[n]) {
+          uis[[j]] <- ui[, which(clusters == j)]
+        }
+      }
+
+      if (sim == TRUE) {
+        # similarity matrix for each segmented ui matrix
+        sims <- replicate(n_range[n], c())
+        for (j in 1:n_range[n]) {
+          sims[[j]] <- metric(uis[[j]], user)
+        }
+      } else {
+        sims <- NA
+      }
+
+      time <- Sys.time() - t1
+      print(time)
+      scores$offline[n] <- scores$offline[n] + time
+
+      print(paste("Online phase:"))
+      t1 <- Sys.time()
+
+      r_pred <- pred_fold_clust_whole(df, cval_f_i[[i]], uis, pred_func,
+                                      clusters, sims, user)
+
+      r_true <- df$rating[cval_f_i[[i]]]
+
+      # error metrics
+      scores$rmse[n] <- scores$rmse[n] + rmse(r_pred, r_true) # nolint
+      scores$mae[n] <- scores$mae[n] + mae(r_pred, r_true) # nolint
+      scores$r2[n] <- scores$r2[n] + r2(r_pred, r_true) # nolint
+
+      time <- Sys.time() - t1
+      print(time)
+      scores$online[n] <- scores$online[n] + time
+
+    }
+  }
+  scores <- scores / t
   return(scores)
 }
--- R Code/Mixed Clustering/Results/Mixed_clustering_results.r
+++ R Code/Mixed Clustering/Results/Mixed_clustering_results.r
@@ -12,26 +12,29 @@ ml100k_feat_d <- read.csv("M4R_Clustering/Data/ml100k_feat_d.csv")
 # call functions
 source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
 source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
+source("M4R_Clustering/R Code/Clustering/Clustering_predictors.r")
 source("M4R_Clustering/R Code/Mixed Clustering/Mixed_clustering_functions.r")
 source("M4R_Clustering/R Code/Mixed Clustering/Mixed_clustering.r")
 
 # initialise evaluation fixed variables
 krange <- seq(from = 10, to = 300, by = 10)
+nrange <- 2:10
+best_n <- c(7, 5, 7, 5, 6, 6, 6, 5)
 
 # evaluate mixed clustering over a k range
 gow_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 7, krange, gen_acos_sim,
                           mean_centered, gow_pam)
 hl_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 5, krange, gen_acos_sim,
                          mean_centered, hl_pam)
-kproto_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 5, krange, gen_acos_sim,
+kproto_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 7, krange, gen_acos_sim,
                              mean_centered, kprototypes)
-mk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 4, krange, gen_acos_sim,
+mk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 5, krange, gen_acos_sim,
                          mean_centered, mixed_k)
-msk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 7, krange, gen_acos_sim,
+msk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 6, krange, gen_acos_sim,
                           mean_centered, mskmeans)
-famd_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 7, krange, gen_acos_sim,
+famd_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 6, krange, gen_acos_sim,
                            mean_centered, famd)
-mrk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 4, krange, gen_acos_sim,
+mrk_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 6, krange, gen_acos_sim,
                           mean_centered, mrkmeans)
 kam_u <- cval_mixed_clust(ml100k, ml100k_dem, 10, 5, krange, gen_acos_sim,
                           mean_centered, kamila_clust)
@@ -46,22 +49,67 @@ write.csv(mclust_u, file =
             "M4R_Clustering/Results/Mixed clustering/Crisp/mclust_u.csv",
           row.names = FALSE)
 
+gow_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, gow_pam, FALSE)
+hl_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, hl_pam, FALSE)
+kproto_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, kprototypes, FALSE)
+mk_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, mixed_k, FALSE)
+msk_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, mskmeans, FALSE)
+famd_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, famd, FALSE)
+mrk_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, mrkmeans, FALSE)
+kam_avg_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, average_clust, kamila_clust, FALSE)
+
+mclust_avg_u <- rbind(gow_avg_u, hl_avg_u, kproto_avg_u, mk_avg_u, msk_avg_u,
+                      famd_avg_u, mrk_avg_u, kam_avg_u)
+mclust_avg_u <- cbind(method = c(rep("gow", 9), rep("hl", 9), rep("kproto", 9),
+                                 rep("mk", 9), rep("msk", 9), rep("famd", 9),
+                                 rep("mrk", 9), rep("kamila", 9)),
+                      mclust_avg_u)
+
+# write user clustering predictor results into file
+write.csv(mclust_avg_u,
+          "M4R_Clustering/Results/Rating clustering/Crisp/mclust_avg_u.csv",
+          row.names = FALSE)
+
+gow_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, gow_pam, TRUE)
+hl_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, hl_pam, TRUE)
+kproto_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, kprototypes, TRUE)
+mk_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, mixed_k, TRUE)
+msk_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, mskmeans, TRUE)
+famd_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, famd, TRUE)
+mrk_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, mrkmeans, TRUE)
+kam_mcent_u <- cval_mixed_clust_pred(ml100k, ml100k_dem, 10, nrange, gen_acos_sim, mean_centered_clust, kamila_clust, TRUE)
+
+mclust_mcent_u <- rbind(gow_mcent_u, hl_mcent_u, kproto_mcent_u, mk_mcent_u,
+                        msk_mcent_u, famd_mcent_u, mrk_mcent_u, kam_mcent_u)
+mclust_mcent_u <- cbind(method = c(rep("gow", 9), rep("hl", 9),
+                                   rep("kproto", 9), rep("mk", 9),
+                                   rep("msk", 9), rep("famd", 9),
+                                   rep("mrk", 9), rep("kamila", 9)),
+                        mclust_mcent_u)
+
+# write user clustering predictor results into file
+write.csv(mclust_mcent_u,
+          "M4R_Clustering/Results/Rating clustering/Crisp/mclust_mcent_u.csv",
+          row.names = FALSE)
+
+best_n <- c(3, 4, 7, 5, 6, 8, 9, 3)
+
 # evaluate mixed clustering over a k range
 gow_i <- cval_mixed_clust(ml100k, ml100k_feat_b, 10, 3, krange, gen_acos_sim,
                           mean_centered, gow_pam, FALSE)
 hl_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 4, krange, gen_acos_sim,
                          mean_centered, hl_pam, FALSE)
-kproto_i <- cval_mixed_clust(ml100k, ml100k_feat_b, 10, 5, krange, gen_acos_sim,
+kproto_i <- cval_mixed_clust(ml100k, ml100k_feat_b, 10, 7, krange, gen_acos_sim,
                              mean_centered, kprototypes, FALSE)
-mk_i <- cval_mixed_clust(ml100k, ml100k_feat_c, 10, 6, krange, gen_acos_sim,
+mk_i <- cval_mixed_clust(ml100k, ml100k_feat_c, 10, 5, krange, gen_acos_sim,
                          mean_centered, mixed_k, FALSE)
-msk_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 5, krange, gen_acos_sim,
+msk_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 6, krange, gen_acos_sim,
                           mean_centered, mskmeans, FALSE)
-famd_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 7, krange, gen_acos_sim,
+famd_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 8, krange, gen_acos_sim,
                            mean_centered, famd, FALSE)
-mrk_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 5, krange, gen_acos_sim,
+mrk_i <- cval_mixed_clust(ml100k, ml100k_feat_d, 10, 9, krange, gen_acos_sim,
                           mean_centered, mrkmeans, FALSE)
-kam_i <- cval_mixed_clust(ml100k, ml100k_feat_b, 10, 5, krange, gen_acos_sim,
+kam_i <- cval_mixed_clust(ml100k, ml100k_feat_b, 10, 3, krange, gen_acos_sim,
                           mean_centered, kamila_clust, FALSE)
 
 # write user mixed clustering results into file
@@ -72,4 +120,47 @@ mclust_i <- cbind(method = c(rep("gow", 30), rep("hl", 30), rep("kproto", 30),
                   mclust_i)
 write.csv(mclust_i, file =
             "M4R_Clustering/Results/Mixed clustering/Crisp/mclust_i.csv",
+          row.names = FALSE)
+
+gow_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, average_clust, gow_pam, FALSE, FALSE)
+hl_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, average_clust, hl_pam, FALSE, FALSE)
+kproto_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, average_clust, kprototypes, FALSE, FALSE)
+mk_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_c, 10, nrange, gen_acos_sim, average_clust, mixed_k, FALSE, FALSE)
+msk_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, average_clust, mskmeans, FALSE, FALSE)
+famd_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, average_clust, famd, FALSE, FALSE)
+mrk_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, average_clust, mrkmeans, FALSE, FALSE)
+kam_avg_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, average_clust, kamila_clust, FALSE, FALSE)
+
+mclust_avg_i <- rbind(gow_avg_i, hl_avg_i, kproto_avg_i, mk_avg_i, msk_avg_i,
+                      famd_avg_i, mrk_avg_i, kam_avg_i)
+mclust_avg_i <- cbind(method = c(rep("gow", 9), rep("hl", 9), rep("kproto", 9),
+                                 rep("mk", 9), rep("msk", 9), rep("famd", 9),
+                                 rep("mrk", 9), rep("kamila", 9)),
+                      mclust_avg_i)
+
+# write user clustering predictor results into file
+write.csv(mclust_avg_i,
+          "M4R_Clustering/Results/Rating clustering/Crisp/mclust_avg_i.csv",
+          row.names = FALSE)
+
+gow_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, mean_centered_clust, gow_pam, TRUE, FALSE)
+hl_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, mean_centered_clust, hl_pam, TRUE, FALSE)
+kproto_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, mean_centered_clust, kprototypes, TRUE, FALSE)
+mk_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_c, 10, nrange, gen_acos_sim, mean_centered_clust, mixed_k, TRUE, FALSE)
+msk_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, mean_centered_clust, mskmeans, TRUE, FALSE)
+famd_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, mean_centered_clust, famd, TRUE, FALSE)
+mrk_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_d, 10, nrange, gen_acos_sim, mean_centered_clust, mrkmeans, TRUE, FALSE)
+kam_mcent_i <- cval_mixed_clust_pred(ml100k, ml100k_feat_b, 10, nrange, gen_acos_sim, mean_centered_clust, kamila_clust, TRUE, FALSE)
+
+mclust_mcent_i <- rbind(gow_mcent_i, hl_mcent_i, kproto_mcent_i, mk_mcent_i,
+                        msk_mcent_i, famd_mcent_i, mrk_mcent_i, kam_mcent_i)
+mclust_mcent_i <- cbind(method = c(rep("gow", 9), rep("hl", 9),
+                                   rep("kproto", 9), rep("mk", 9),
+                                   rep("msk", 9), rep("famd", 9),
+                                   rep("mrk", 9), rep("kamila", 9)),
+                        mclust_mcent_i)
+
+# write user clustering predictor results into file
+write.csv(mclust_mcent_i,
+          "M4R_Clustering/Results/Rating clustering/Crisp/mclust_mcent_i.csv",
           row.names = FALSE)

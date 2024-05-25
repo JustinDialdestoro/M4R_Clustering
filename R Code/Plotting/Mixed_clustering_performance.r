a9477940c03a6fec60be180503e573d2bf9f5fc7
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-05-26 00:21:25 +0100

    crisp mixed clustering results

--- R Code/Plotting/Mixed_clustering_performance.r
+++ R Code/Plotting/Mixed_clustering_performance.r
@@ -1,169 +1,83 @@
-# load unclustered performance results
-loc <- "M4R_Clustering/Results/Collaborative Filtering/pred_ups.csv"
-pred_ups <- read.csv(loc)
-noclust_u <- pred_ups[pred_ups$predictor == "mean centred", ][2:6]
-loc <- "M4R_Clustering/Results/Rating clustering/Crisp/pref_clust_ups.csv"
-pref_clust_ups <- read.csv(loc)
-pref_clust_u <- pref_clust_ups[pref_clust_ups$predictor ==
-                                 "mean centred", ][2:6]
-loc <- "M4R_Clustering/Results/Rating clustering/Crisp/clust_u.csv"
-clust_u <- read.csv(loc)
+# load colour package
+library("scales")
 
-full <- rbind(noclust_u, pref_clust_u, clust_u,
-              gow_u, hl_u, kproto_u, mk_u, msk_u, famd_u, mrk_u, kam_u)
-
-ymax <- max(full$rmse)
-ymin <- min(full$rmse)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$rmse, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "RMSE",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:10) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$rmse,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 1])
-}
-legend("topright",
-       c("no clustering", "preference clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)),
-       lty = 1, lwd = 2, cex = 0.5)
-
-ymax <- max(full$mae)
-ymin <- min(full$mae)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$mae, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "MAE",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:10) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$mae,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 1])
-}
-legend("topright",
-       c("no clustering", "preference clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)),
-       lty = 1, lwd = 2, cex = 0.5)
-
-ymax <- max(full$r2)
-ymin <- min(full$r2)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$r2, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "R2",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:10) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$r2,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 1])
-}
-legend("bottomright",
-       c("no clustering", "preference clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)),
-       lty = 1, lwd = 2, cex = 0.5)
-
-ymax <- max(full$online)
-ymin <- min(full$online)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$online, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours",
-     ylab = "Online phase time (seconds)",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:10) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$online,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 1])
-}
-legend("topleft",
-       c("no clustering", "preference clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)),
-       lty = 1, lwd = 2, cex = 0.5)
-
-# load unclustered performance results
+# read in the data
+loc <- "M4R_Clustering/Results/Mixed clustering/Crisp/mclust_u.csv"
+mclust_u <- read.csv(loc)
+loc <- "M4R_Clustering/Results/Mixed clustering/Crisp/mclust_i.csv"
+mclust_i <- read.csv(loc)
+loc <- "M4R_Clustering/Results/Collaborative Filtering/pred_u.csv"
+pred_u <- read.csv(loc)
 loc <- "M4R_Clustering/Results/Collaborative Filtering/pred_i.csv"
 pred_i <- read.csv(loc)
-noclust_i <- pred_i[pred_i$predictor == "mean centred", ][2:6]
+loc <- "M4R_Clustering/Results/Rating clustering/Crisp/clust_u.csv"
+clust_u <- read.csv(loc)
 loc <- "M4R_Clustering/Results/Rating clustering/Crisp/clust_i.csv"
 clust_i <- read.csv(loc)
 
-full <- rbind(noclust_i, clust_i,
-              gow_i, hl_i, kproto_i, mk_i, msk_i, famd_i, mrk_i, kam_i)
+# initialise plotting variables
+krange <- seq(from = 10, to = 300, by = 10)
+metric_labels <- c("RMSE", "MAE", "R2", "Online phase time (seconds)")
+index <- c(2, 3, 4, 6)
+best_n_u <- c(7, 5, 7, 5, 6, 6, 6, 5)
+best_n_i <- c(3, 4, 7, 5, 6, 8, 9, 3)
 
-ymax <- max(full$rmse)
-ymin <- min(full$rmse)
-ygap <- 0.2 * (ymax - ymin)
+# prepare results
+noclust_u <- pred_u[61:90, ]
+names(noclust_u)[1] <- "clustering"
+noclust_u[1] <- "none"
+clust_u <- cbind(clustering = "ratings", clust_u)
+names(mclust_u)[1] <- "clustering"
+full_u <- rbind(noclust_u, clust_u, mclust_u)
 
-plot(krange, full[1:30, ]$rmse, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "RMSE",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:9) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$rmse,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 2])
+for (i in 1:4) {
+  ymax <- max(full_u[, index[i]])
+  ymin <- min(full_u[, index[i]])
+  ygap <- 0.1 * (ymax - ymin)
+
+  plot(krange, full_u[1:30, index[i]], lty = 1, type = "l", lwd = 2,
+       col = hue_pal()(10)[1], xlab = "k neighbours", ylab = metric_labels[i],
+       ylim = c(ymin - ygap, ymax + ygap))
+
+  for (j in 1:10) {
+    lines(krange, full_u[(j * 30 + 1):((j + 1) * 30), index[i]],
+          lty = 1, type = "l", lwd = 2, col = hue_pal()(10)[j + 1])
+  }
+
+  legend("topright",
+         c("no clustering", "rating clustering", "Gower/PAM", "HL/PAM",
+           "K-prototypes", "Mixed K-Means", "MS K-Means", "FAMD", "Mixed RKM",
+           "KAMILA"),
+         col = c(hue_pal()(10)),
+         lty = 1, lwd = 2, cex = 0.5)
 }
-legend("topright",
-       c("no clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)[-2]),
-       lty = 1, lwd = 2, cex = 0.5)
 
-ymax <- max(full$mae)
-ymin <- min(full$mae)
-ygap <- 0.2 * (ymax - ymin)
+# prepare results
+noclust_i <- pred_i[61:90, ]
+names(noclust_i)[1] <- "clustering"
+noclust_i[1] <- "none"
+clust_i <- cbind(clustering = "ratings", clust_i)
+names(mclust_i)[1] <- "clustering"
+full_i <- rbind(noclust_i, clust_i, mclust_i)
 
-plot(krange, full[1:30, ]$mae, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "MAE",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:9) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$mae,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 2])
+for (i in 1:4) {
+  ymax <- max(full_i[, index[i]])
+  ymin <- min(full_i[, index[i]])
+  ygap <- 0.1 * (ymax - ymin)
+
+  plot(krange, full_i[1:30, index[i]], lty = 1, type = "l", lwd = 2,
+       col = hue_pal()(10)[1], xlab = "k neighbours", ylab = metric_labels[i],
+       ylim = c(ymin - ygap, ymax + ygap))
+
+  for (j in 1:10) {
+    lines(krange, full_i[(j * 30 + 1):((j + 1) * 30), index[i]],
+          lty = 1, type = "l", lwd = 2, col = hue_pal()(10)[j + 1])
+  }
+
+  legend("topright",
+         c("no clustering", "rating clustering", "Gower/PAM", "HL/PAM",
+           "K-prototypes", "Mixed K-Means", "MS K-Means", "FAMD", "Mixed RKM",
+           "KAMILA"),
+         col = c(hue_pal()(10)),
+         lty = 1, lwd = 2, cex = 0.5)
 }
-legend("topright",
-       c("no clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)[-2]),
-       lty = 1, lwd = 2, cex = 0.5)
-
-ymax <- max(full$r2)
-ymin <- min(full$r2)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$r2, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours", ylab = "R2",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:9) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$r2,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 2])
-}
-legend("topright",
-       c("no clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)[-2]),
-       lty = 1, lwd = 2, cex = 0.5)
-
-ymax <- max(full$online)
-ymin <- min(full$online)
-ygap <- 0.2 * (ymax - ymin)
-
-plot(krange, full[1:30, ]$online, lty = 1, type = "l", lwd = 2,
-     col = hue_pal()(11)[1], xlab = "k neighbours",
-     ylab = "Online phase time (seconds)",
-     ylim = c(ymin - ygap, ymax + ygap))
-for (i in 1:9) {
-  lines(krange, full[(i * 30 + 1):((i + 1) * 30), ]$online,
-        lty = 1, type = "l", lwd = 2, col = hue_pal()(11)[i + 2])
-}
-legend("topleft",
-       c("no clustering", "rating clustering",
-         "Gower/PAM", "HL/PAM", "K-prototypes", "Mixed K-Means", "MS K-Means",
-         "FAMD", "Mixed RKM", "KAMILA"),
-       col = c(hue_pal()(11)[-2]),
-       lty = 1, lwd = 2, cex = 0.5)

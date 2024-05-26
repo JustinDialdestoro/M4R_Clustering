581df306fb63983064ca3e6d51fe407e4c6097dc
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-05-27 00:14:41 +0100

    no similarity fuzzy user results

--- R Code/Fuzzy Clustering/Fuzzy_clustering.r
+++ R Code/Fuzzy Clustering/Fuzzy_clustering.r
@@ -52,6 +52,10 @@ cval_mixed_fclust <- function(df, df_feat, t, n, m, k_range, metric, pred_func,
       sim[l, ] <- sim[l, ] * clusters[inds[l], ]
     }
 
+    time <- Sys.time() - t1
+    print(time)
+    scores$offline[i] <- time
+
     # loop over every k
     for (k in seq_along(k_range)) {
       print(paste("Online phase for k =", k_range[k]))
@@ -76,8 +80,8 @@ cval_mixed_fclust <- function(df, df_feat, t, n, m, k_range, metric, pred_func,
   return(scores)
 }
 
-pred_fold_fclust <- function(df, df_ind, uis, sims, pred_func, k, clusters,
-                             n, inds, user = TRUE) {
+pred_fold_split <- function(df, df_ind, uis, sims, pred_func, k, clusters,
+                            n, inds, user = TRUE) {
   preds <- c()
 
   if (user == TRUE) {
@@ -175,14 +179,133 @@ cval_mixed_fclust_split <- function(df, df_feat, t, n, m, k_range, metric,
       sims[[j]] <- metric(uis[[j]], user)
     }
 
+    time <- Sys.time() - t1
+    print(time)
+    scores$offline[i] <- time
+
     # loop over every k
     for (k in seq_along(k_range)) {
       print(paste("Online phase for k =", k_range[k]))
       t1 <- Sys.time()
 
       # predict on test fold ratings
-      r_pred <- pred_fold_fclust(df, cval_f_i[[i]], uis, sims, pred_func, # nolint
-                                 k_range[k], clusters, n, inds, user)
+      r_pred <- pred_fold_split(df, cval_f_i[[i]], uis, sims, pred_func,
+                                k_range[k], clusters, n, inds, user)
+      r_true <- df$rating[cval_f_i[[i]]]
+
+      # error metrics
+      scores$rmse[k] <- scores$rmse[k] + rmse(r_pred, r_true) # nolint
+      scores$mae[k] <- scores$mae[k] + mae(r_pred, r_true) # nolint
+      scores$r2[k] <- scores$r2[k] + r2(r_pred, r_true) # nolint
+
+      time <- Sys.time() - t1
+      print(time)
+      scores$online[k] <- scores$online[k] + time
+    }
+  }
+  scores[c(1:3, 5)] <- scores[c(1:3, 5)] / t
+  return(scores)
+}
+
+fclust_pred <- function(ui, k, userid, filmid, clusters, c, user = TRUE) {
+  if (user == TRUE) {
+    # indices of users who have rated the film
+    ind <- which(ui[, filmid] > 0)
+    # find k users most belonging to this cluster
+    neighbours <- ind[order(-clusters[c, ind])[1:k]]
+
+    # predict using user weights
+    pred <- sum(clusters[c, neighbours] * ui[neighbours, filmid]) /
+      sum(clusters[c, neighbours])
+
+  } else {
+    # indices of films which have been rated by the user
+    ind <- which(ui[userid, ] > 0)
+    # find k users most belonging to this cluster
+    neighbours <- ind[order(-clusters[c, ind])[1:k]]
+
+    # predict using user weights
+    pred <- sum(clusters[c, neighbours] * ui[userid, neighbours]) /
+      sum(clusters[c, neighbours])
+  }
+
+  return(pred)
+}
+
+pred_fold_nosim <- function(df, df_ind, ui, k, clusters, n, user = TRUE) {
+  preds <- c()
+
+  if (user == TRUE) {
+    # compute rating prediction for every test case
+    for (p in df_ind) {
+      preds_i <- 0
+
+      # target prediction id
+      userid <- df$userID[p]
+      filmid <- df$filmID[p]
+
+      # loop over each cluster
+      for (c in 1:n) {
+        # prediction
+        preds_i <- preds_i + clusters[c, userid] *
+          fclust_pred(ui, k, userid, filmid, clusters, c)
+      }
+      preds <- c(preds, preds_i / sum(clusters[, userid]))
+    }
+  } else {
+    # compute rating prediction for every test case
+    for (p in df_ind) {
+      preds_i <- 0
+
+      # target prediction id
+      userid <- df$userID[p]
+      filmid <- df$filmID[p]
+
+      for (c in 1:n) {
+        # prediction
+        preds_i <- preds_i + clusters[c, filmid] *
+          fclust_pred(ui, k, userid, filmid, clusters, c, user)
+      }
+      preds <- c(preds, preds_i / sum(clusters[, filmid]))
+    }
+  }
+  return(preds)
+}
+
+cval_mixed_fclust_nosim <- function(df, df_feat, t, n, m, k_range, clust_func,
+                                    user = TRUE) {
+  nk <- length(k_range)
+  # initial scores table
+  scores <- data.frame(rmse = rep(0, nk), mae = rep(0, nk), r2 = rep(0, nk),
+                       offline = rep(0, t), online = rep(0, nk))
+
+  # t-fold creation
+  cval_f_i <- t_fold_index(df, t, user) # nolint
+  cval_f <- t_fold(df, cval_f_i) # nolint
+
+  # loop over each fold
+  for (i in 1:t) {
+    print(paste("Offline phase for fold", i, ":"))
+    t1 <- Sys.time()
+
+    # ui and similarity matrix
+    ui <- gen_ui_matrix(df, cval_f[[i]]) # nolint
+
+    # create user clusters
+    clusters <- clust_func(df_feat, n, m, user)$clusters
+
+    time <- Sys.time() - t1
+    print(time)
+    scores$offline[i] <- time
+
+    # loop over every k
+    for (k in seq_along(k_range)) {
+      print(paste("Online phase for k =", k_range[k]))
+      t1 <- Sys.time()
+
+      # predict on test fold ratings
+      r_pred <- pred_fold_nosim(df, cval_f_i[[i]], ui, k_range[k], clusters, n,
+                                user)
       r_true <- df$rating[cval_f_i[[i]]]
 
       # error metrics
---
+++ R Code/Fuzzy Clustering/Results/Fuzzy_nosim_results.r
@@ -0,0 +1,81 @@
+# load colour package
+library("scales")
+
+# read in the data
+ml100k <- read.csv("M4R_Clustering/Data/ml100k.csv")
+ml100k_dem <- read.csv("M4R_Clustering/Data/ml100k_dem.csv")
+ml100k_feat_b <- read.csv("M4R_Clustering/Data/ml100k_feat_b.csv")
+ml100k_feat_c <- read.csv("M4R_Clustering/Data/ml100k_feat_c.csv")
+ml100k_feat_d <- read.csv("M4R_Clustering/Data/ml100k_feat_d.csv")
+
+# call functions
+source("M4R_Clustering/R Code/Fuzzy Clustering/Mixed_fuzzy_functions.r")
+source("M4R_Clustering/R Code/Fuzzy Clustering/Fuzzy_clustering.r")
+source("M4R_Clustering/R Code/Collaborative Filtering/CF.r")
+source("M4R_Clustering/R Code/Collaborative Filtering/Similarities.r")
+source("M4R_Clustering/R Code/Collaborative Filtering/Predictors.r")
+
+# initialise evaluation fixed variables
+krange <- seq(from = 10, to = 300, by = 10)
+best_n_u <- c(5, 5, 6, 3, 4, 3, 11)
+
+# evaluate mixed clustering over a k range
+gow_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 5, 2, krange,
+                                 fuzzy_gow)
+hl_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 5, 2, krange,
+                                fuzzy_hl)
+kproto_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 6, 2, krange,
+                                    fuzzy_kproto)
+mk_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 3, 2, krange,
+                                fuzzy_mixed_k)
+msk_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 4, 2, krange,
+                                 fuzzy_mskmeans)
+famd_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 3, 2, krange,
+                                  fuzzy_famd)
+mrk_u <- cval_mixed_fclust_nosim(ml100k, ml100k_dem, 10, 3, 2, krange,
+                                 fuzzy_mrkmeans)
+
+# write user mixed clustering results into file
+fclust_nosim_u <- rbind(gow_u, hl_u, kproto_u, mk_u, msk_u, famd_u, mrk_u)
+fclust_nosim_u <- cbind(method = c(rep("gow", 30), rep("hl", 30),
+                                   rep("kproto", 30), rep("mk", 30),
+                                   rep("msk", 30), rep("famd", 30),
+                                   rep("mrk", 30)),
+                        fclust_nosim_u)
+write.csv(fclust_nosim_u, file =
+            "M4R_Clustering/Results/Mixed clustering/Fuzzy/fclust_nosim_u.csv",
+          row.names = FALSE)
+
+best_n_i <- c(6, 5, 5, 4, 3, 3, 11)
+
+# evaluate mixed clustering over a k range
+gow_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_b, 10, 6, 2, krange,
+                                 fuzzy_gow, FALSE)
+hl_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_d, 10, 5, 2, krange,
+                                fuzzy_hl, FALSE)
+kproto_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_c, 10, 5, 2, krange,
+                                    fuzzy_kproto,
+                                    FALSE)
+mk_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_c, 10, 4, 2, krange,
+                                fuzzy_mixed_k,
+                                FALSE)
+msk_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_d, 10, 3, 2, krange,
+                                 fuzzy_mskmeans,
+                                 FALSE)
+famd_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_d, 10, 8, 2, krange,
+                                  fuzzy_famd,
+                                  FALSE)
+mrk_i <- cval_mixed_fclust_nosim(ml100k, ml100k_feat_d, 10, 8, 2, krange,
+                                 fuzzy_mrkmeans,
+                                 FALSE)
+
+# write user mixed clustering results into file
+fclust_nosim_i <- rbind(gow_i, hl_i, kproto_i, mk_i, msk_i, famd_i, mrk_i)
+fclust_nosim_i <- cbind(method = c(rep("gow", 30), rep("hl", 30),
+                                   rep("kproto", 30), rep("mk", 30),
+                                   rep("msk", 30), rep("famd", 30),
+                                   rep("mrk", 30)),
+                        fclust_nosim_i)
+write.csv(fclust_nosim_i, file =
+            "M4R_Clustering/Results/Mixed clustering/Fuzzy/fclust_nosim_i.csv",
+          row.names = FALSE)

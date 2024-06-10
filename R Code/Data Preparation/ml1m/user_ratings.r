71cf8e74de4cbd36e664b18486cd08c95adafe29
Justin Dialdestoro <justin.dialdestoro20@imperial.ac.uk>
2024-06-10 09:18:03 +0100

    removed not used results

--- R Code/Data Preparation/ml1m/user_ratings.r
+++
@@ -1,6 +0,0 @@
-# read 1m rating data
-u1m <- read.csv("Data/ml-1m/ml-1m/ratings.dat", sep = ":",
-                colClasses = c(NA, "NULL"), header = FALSE)
-
-# column names for 1m dataset
-colnames(u1m) <- c("userID", "filmID", "rating", "timestamp")
